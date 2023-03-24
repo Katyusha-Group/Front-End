27b2e0b0f556bce120b20b3d9e5abbdbde594679
yazdan <ymasteryfarahani@gmail.com>
2023-03-24 19:12:06 +0430

    Login page with bugs
    Some bugs to add image in page

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,6 @@
 
-import React from "react";
 
+import React from "react";
 // reactstrap components
 import {
   Button,
@@ -21,170 +21,50 @@ function Login() {
     <>
       <div className="content" >
         <Row>
-          <Col md="8">
+          <Col className="text-right" md="8">
             <Card>
               <CardHeader>
-                <h5 className="title">Login</h5>
+                <h5 className="title">ورود به سایت</h5>
               </CardHeader>
               <CardBody>
                 <Form>
                   <Row>
-                    {/* <Col className="pr-md-1" md="5">
-                      <FormGroup>
-                        <label>Full Name </label>
-                        <Input
-                          defaultValue="Creative Code Inc."
-                        //   disabled
-                          placeholder="Full Name"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col> */}
-                    {/* <Col className="pr-md-1" md="3">
-                      <FormGroup>
-                        <label>Username</label>
-                        <Input
-                          defaultValue="michael23"
-                          placeholder="Username"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col> */}
                     <Col className="pr-md-1" md="6">
+                    {/* <img
+                      alt="..."
+                      className="avatar"
+                      src={require("assets/img/Login.jpg")}
+                    /> */}
                       <FormGroup>
                         <label htmlFor="exampleInputEmail1">
-                          Email address
+                          ایمیل
                         </label>
-                        <Input placeholder="mike@email.com" type="email" />
+                        <Input className="text-left" placeholder="JohnDoe@gmail.com" type="email" />
                       </FormGroup>
                     </Col>
                   </Row>
                   <Row >
-                    <Col className="pr-md-1" md="6">
+                    <Col className="pr-md-1 " md="6">
                       <FormGroup>
-                        <label>Password</label>
+                        <label>رمز عبور</label>
                         <Input
+                        className="text-left"
                         //   defaultValue="Mike"
                           placeholder="Password"
                           type="password"
                         />
                       </FormGroup>
                     </Col>
-                    {/* <Col className="pl-md-1" md="6">
-                      <FormGroup>
-                        <label>Last Name</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Last Name"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col> */}
                   </Row>
-                  {/* <Row>
-                    <Col md="12">
-                      <FormGroup>
-                        <label>Address</label>
-                        <Input
-                          defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                          placeholder="Home Address"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="4">
-                      <FormGroup>
-                        <label>City</label>
-                        <Input
-                          defaultValue="Mike"
-                          placeholder="City"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-md-1" md="4">
-                      <FormGroup>
-                        <label>Country</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Country"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="4">
-                      <FormGroup>
-                        <label>Postal Code</label>
-                        <Input placeholder="ZIP Code" type="number" />
-                      </FormGroup>
-                    </Col>
-                  </Row> */}
-                  {/* <Row>
-                    <Col md="8">
-                      <FormGroup>
-                        <label>About Me</label>
-                        <Input
-                          cols="80"
-                          defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                            that two seat Lambo."
-                          placeholder="Here can be your description"
-                          rows="4"
-                          type="textarea"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row> */}
                 </Form>
               </CardBody>
               <CardFooter>
                 <Button className="btn-fill" color="primary" type="submit">
-                  Login
+                  ورود
                 </Button>
               </CardFooter>
             </Card>
           </Col>
-          {/* <Col md="4">
-            <Card className="card-user">
-              <CardBody>
-                <CardText />
-                <div className="author">
-                  <div className="block block-one" />
-                  <div className="block block-two" />
-                  <div className="block block-three" />
-                  <div className="block block-four" />
-                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                    <img
-                      alt="..."
-                      className="avatar"
-                      src={require("assets/img/emilyz.jpg")}
-                    />
-                    <h5 className="title">Mike Andrew</h5>
-                  </a>
-                  <p className="description">Ceo/Co-Founder</p>
-                </div>
-                <div className="card-description">
-                  Do not be scared of the truth because we need to restart the
-                  human foundation in truth And I love you like Kanye loves
-                  Kanye I love Rick Owens’ bed design but the back is...
-                </div>
-              </CardBody>
-              <CardFooter>
-                <div className="button-container">
-                  <Button className="btn-icon btn-round" color="facebook">
-                    <i className="fab fa-facebook" />
-                  </Button>
-                  <Button className="btn-icon btn-round" color="twitter">
-                    <i className="fab fa-twitter" />
-                  </Button>
-                  <Button className="btn-icon btn-round" color="google">
-                    <i className="fab fa-google-plus" />
-                  </Button>
-                </div>
-              </CardFooter>
-            </Card>
-          </Col> */}
         </Row>
       </div>
     </>
@@ -192,3 +72,5 @@ function Login() {
 }
 
 export default Login;
+
+
