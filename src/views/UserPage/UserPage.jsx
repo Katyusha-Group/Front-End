7ec289304403838c2d513a46e52c6d72f4f79d74
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-04-08 13:50:43 +0330

    One bug in UserPage solved

--- src/views/UserPage/UserPage.jsx
+++ src/views/UserPage/UserPage.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import classNames from "classnames";
-import { Line, Bar } from "react-chartjs-2";
+// import { Line, Bar } from "react-chartjs-2";
 import {
   Button,
   ButtonGroup,
