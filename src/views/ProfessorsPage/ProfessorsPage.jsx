d9b2515673990f1927d1fadacfd689bf5dea0426
shakiba <shakiba.anarakifirooz@gmail.com>
2023-04-16 09:53:10 +0330

    set sum cards

--- src/views/ProfessorsPage/ProfessorsPage.jsx
+++ src/views/ProfessorsPage/ProfessorsPage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+// import {Select} from 'react-select';
 import { useState } from "react";
+import "../ProfessorsPage/ProfessorsPage.css";
+
 
 // reactstrap components
 import {
@@ -20,65 +23,32 @@ import {
   Col,
   Container,
 } from "reactstrap";
+const optionList = [
+  { value: "red", label: "Red" },
+  { value: "green", label: "Green" },
+  { value: "yellow", label: "Yellow" },
+  { value: "blue", label: "Blue" },
+  { value: "white", label: "White" },
+];
 
 export default function ProfessorsPage() {
-    function handleSelect(e) {
-        setFrequency((prev) => ({
-          ...prev,
-          [e.target.name]: e.target.value,
-        }));
-      }
+  // function handleSelect(e) {
+  //     setFrequency((prev) => ({
+  //       ...prev,
+  //       [e.target.name]: e.target.value,
+  //     }));
+  //   }
+  
+
   return (
     <>
-      <div>
-        <Card className="coursesCard">
-          <CardBody className="text-right">
-            <div md="12" style={{ marginBottom: "10px" }}>
-              <label>رشته</label>
-              <br />
-              <FormGroup>
-                <Input
-                  type="select"
-                  name="frequency"
-                  id="frequency"
-                //value={frequency.frequency}
-                  onChange={handleSelect}
-                >
-                  <option>انتخاب کنید</option>
-                  <option>مهندسی کامپیوتر</option>
-                  <option>مهندسی برق</option>
-                  <option>مهندسی مکانیک</option>
-                  <option>مهندسی شیمی</option>
-                  <option>مهندسی عمران</option>
-                  <option>مهندسی راه آهن</option>
-                  <option>مهندسی صنایع</option>
-                  <option>مهندسی معماری</option>
-                  <option>مهندسی مواد</option>
-                  <option>علوم کامپیوتر</option>
-                  <option>ریاضی</option>
-                  <option>فیزیک</option>
-                </Input>
-              </FormGroup>
-
-              {/* <Col md="6">
-                <FormGroup>
-                  <br />
-                  <Input
-                    type="select"
-                    name="frequency"
-                    id="frequency"
-                    value={frequency.frequency}
-                    onChange={handleSelect}
-                  >
-                    <option>زن</option>
-                    <option>مرد</option>
-                  </Input>
-                </FormGroup>
-              </Col> */}
-            </div>
-          </CardBody>
-        </Card>
-      </div>
+      {/* const { selectedOption } = this.state; */}
+        {/* <Select
+          value={selectedOption}
+          // onChange={this.handleChange}
+          options={optionList}
+        /> */}
+        <label>dhjh</label>
     </>
   );
 }
