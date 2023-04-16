59e07c61521b75d585f8511c583c9ce7f7e39631
Bahareh Kavousi <kavousi.b99@gmail.com>
2023-04-17 01:06:23 +0330

    Convert xslx to json

--- src/views/CoursesPanel/CoursesPanel.jsx
+++ src/views/CoursesPanel/CoursesPanel.jsx
@@ -8,12 +8,58 @@ import {
   Button,
   Row,
   Col,
+  Input,
 } from "reactstrap";
+import * as XLSX from 'xlsx';
 
+
+function upload() {
+  //alert("Upload clicked");
+  var files = document.getElementById('file_upload').files;
+
+  if(files.length==0){
+    alert("Please choose any file...");
+    return;
+  }
+  var filename = files[0].name;
+  var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
+  if (extension == '.XLS' || extension == '.XLSX') {
+      excelFileToJSON(files[0]);
+  }else{
+      alert("Please select a valid excel file.");
+  }
+}
+function excelFileToJSON(file){
+  try {
+    var reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = function(e) {
+
+        var data = e.target.result;
+        var workbook = XLSX.read(data, {
+            type : 'binary'
+        });
+        var result = {};
+        workbook.SheetNames.forEach(function(sheetName) {
+            var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+            if (roa.length > 0) {
+                result[sheetName] = roa;
+            }
+        });
+        //displaying the json result
+        var resultEle=document.getElementById("json-result");
+        resultEle.value=JSON.stringify(result, null, 4);
+        resultEle.style.display='block';
+        }
+    }catch(e){
+        console.error(e);
+    }
+}
 function CoursesPanel() {
   return (
     <>
-    <div className="div-1">
+
+    {/* <div className="div-1">
     
     <span >
       <div className="div-2">
@@ -35,6 +81,13 @@ function CoursesPanel() {
     <span>
       
     </span>
+    <Button>Hello</Button> */}
+    <h1>Upload an excel file to convert into JSON</h1>
+    {/* <input type="file" id="file_upload" /> */}
+    <Input type='file' id="file_upload"></Input>
+    <Button onclick={upload()}>Upload</Button>  
+    <br></br>
+    <textarea id="json-result" style={{display:'none', height:'500px', width:'350px'}}></textarea>
     </>
   );
 }
