6892e6160b792c2f7d4c990d968cf4161cac7b28
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-04-17 12:44:42 +0330

    Sidebar
    In Sidebar we can department and see lessons

---
+++ src/components/LessonSidebar/LessonSidebar.jsx
@@ -0,0 +1,161 @@
+
+/*eslint-disable*/
+import React from "react";
+
+import { NavLink, Link, useLocation } from "react-router-dom";
+
+// nodejs library to set properties for components
+import { PropTypes } from "prop-types";
+
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+
+// reactstrap components
+import { Nav, NavLink as ReactstrapNavLink } from "reactstrap";
+
+import {
+  BackgroundColorContext,
+  backgroundColors
+} from "../../contexts/BackgroundColorContext";
+
+import "./LessonSidebar.css"
+
+import classData from "../../assets/data/data.json"
+
+var ps;
+
+function Sidebar(props) {
+  const location = useLocation();
+  const sidebarRef = React.useRef(null);
+  let [lessonState, setLessonState] = React.useState()
+  // verifies if routeName is the one active (in browser input)
+  const activeRoute = (routeName) => {
+    return location.pathname === routeName ? "active" : "";
+  };
+  React.useEffect(() => {
+    if (navigator.userAgentData.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(sidebarRef.current, {
+        suppressScrollX: true,
+        suppressScrollY: false
+      });
+    }
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      if (navigator.userAgentData.platform.indexOf("Win") > -1) {
+        ps.destroy();
+      }
+    };
+  });
+  
+  const linkOnClick = () => {
+    document.documentElement.classList.remove("nav-open");
+  };
+  const { routes, rtlActive, logo } = props;
+  let logoImg = null;
+  let logoText = null;
+//   rtlActive = true;
+  if (logo !== undefined) {
+    if (logo.outterLink !== undefined) {
+      logoImg = (
+        <a
+          href={logo.outterLink}
+          className="simple-text logo-mini"
+          target="_blank"
+          onClick={props.toggleSidebar}
+        >
+          <div className="logo-img">
+            <img src={logo.imgSrc} alt="react-logo" />
+          </div>
+        </a>
+      );
+      logoText = (
+        <a
+          href={logo.outterLink}
+          className="simple-text logo-normal"
+          target="_blank"
+          onClick={props.toggleSidebar}
+        >
+          {logo.text}
+        </a>
+      );
+    } else {
+      logoImg = (
+        <Link
+          to={logo.innerLink}
+          className="simple-text logo-mini"
+          onClick={props.toggleSidebar}
+        >
+          <div className="logo-img">
+            <img src={logo.imgSrc} alt="react-logo" />
+          </div>
+        </Link>
+      );
+      logoText = (
+        <Link
+          to={logo.innerLink}
+          className="simple-text logo-normal"
+          onClick={props.toggleSidebar}
+        >
+          {logo.text}
+        </Link>
+      );
+    }
+  }
+  return (
+    <BackgroundColorContext.Consumer>
+      {({ color }) => (
+        <div className="sidebar" data={color}>
+          <div className="sidebar-wrapper" ref={sidebarRef}>
+            {logoImg !== null || logoText !== null ? (
+              <div className="logo">
+                {logoImg}
+                {logoText}
+              </div>
+            ) : null}
+            <Nav>
+              <div className="lessonSidebar_component">
+                
+                {classData.map((prop) => (
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="active"
+                    onClick={() => setLessonState(prop.departeman)}
+                  >
+                    <i />
+                    <p>{prop.departeman.name}</p>
+                  </NavLink>
+                ))}
+              </div>
+              <div className="lessonSidebar_component lessonSidebar_component-lessons">
+                {console.log(`hello ${lessonState}`)}
+                {console.log(lessonState)}
+                {lessonState?.base_course.map((prop) => (
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i />
+                    <p>{prop.course_name}</p>
+                  </NavLink>
+                ))}
+              </div>
+            </Nav>
+          </div>
+        </div>
+      )}
+    </BackgroundColorContext.Consumer>
+  );
+}
+
+Sidebar.propTypes = {
+  rtlActive: PropTypes.bool,
+  routes: PropTypes.arrayOf(PropTypes.object),
+  logo: PropTypes.shape({
+    innerLink: PropTypes.string,
+    outterLink: PropTypes.string,
+    text: PropTypes.node,
+    imgSrc: PropTypes.string
+  })
+};
+
+export default Sidebar;
