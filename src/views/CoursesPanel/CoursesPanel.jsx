3ad11fb0626e5be9d2514bf6efaee718a7c6835b
Bahareh Kavousi <kavousi.b99@gmail.com>
2023-04-17 23:14:10 +0330

    Added a new implementation (xslx2json)

--- src/views/CoursesPanel/CoursesPanel.jsx
+++ src/views/CoursesPanel/CoursesPanel.jsx
@@ -9,53 +9,73 @@ import {
   Row,
   Col,
   Input,
+  Form,
+  Label,
 } from "reactstrap";
-import * as XLSX from 'xlsx';
+import * as xlsx from 'xlsx';
 
 
-function upload() {
-  //alert("Upload clicked");
-  var files = document.getElementById('file_upload').files;
 
-  if(files.length==0){
-    alert("Please choose any file...");
-    return;
-  }
-  var filename = files[0].name;
-  var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
-  if (extension == '.XLS' || extension == '.XLSX') {
-      excelFileToJSON(files[0]);
-  }else{
-      alert("Please select a valid excel file.");
-  }
-}
-function excelFileToJSON(file){
-  try {
-    var reader = new FileReader();
-    reader.readAsBinaryString(file);
-    reader.onload = function(e) {
-
-        var data = e.target.result;
-        var workbook = XLSX.read(data, {
-            type : 'binary'
-        });
-        var result = {};
-        workbook.SheetNames.forEach(function(sheetName) {
-            var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-            if (roa.length > 0) {
-                result[sheetName] = roa;
-            }
-        });
-        //displaying the json result
-        var resultEle=document.getElementById("json-result");
-        resultEle.value=JSON.stringify(result, null, 4);
-        resultEle.style.display='block';
-        }
-    }catch(e){
-        console.error(e);
-    }
-}
 function CoursesPanel() {
+  // const upload = () => {
+  //   //alert("Upload clicked");
+  //   var files = document.getElementById('file_upload').files;
+
+  //   if(files.length==0){
+  //     alert("Please choose any file...");
+  //     return;
+  //   }
+  //   var filename = files[0].name;
+  //   var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
+  //   if (extension == '.XLS' || extension == '.XLSX') {
+  //       excelFileToJSON(files[0]);
+  //   }else{
+  //       alert("Please select a valid excel file.");
+  //   }
+  // }
+  // const excelFileToJSON = (file) => {
+  //   try {
+  //     var reader = new FileReader();
+  //     reader.readAsBinaryString(file);
+  //     reader.onload = function(e) {
+  
+  //         var data = e.target.result;
+  //         var workbook = XLSX.read(data, {
+  //             type : 'binary'
+  //         });
+  //         var result = {};
+  //         workbook.SheetNames.forEach(function(sheetName) {
+  //             var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+  //             if (roa.length > 0) {
+  //                 result[sheetName] = roa;
+  //             }
+  //         });
+  //         //displaying the json result
+  //         var resultEle=document.getElementById("json-result");
+  //         resultEle.value=JSON.stringify(result, null, 4);
+  //         resultEle.style.display='block';
+  //         }
+  //     }catch(e){
+  //         console.error(e);
+  //     }
+  // }
+
+
+  const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = xlsx.read(data, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = xlsx.utils.sheet_to_json(worksheet);
+            console.log(json);
+        };
+        reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  }
   return (
     <>
 
@@ -82,15 +102,69 @@ function CoursesPanel() {
       
     </span>
     <Button>Hello</Button> */}
-    <h1>Upload an excel file to convert into JSON</h1>
-    {/* <input type="file" id="file_upload" /> */}
+    {/* <h1>Upload an excel file to convert into JSON</h1>
     <Input type='file' id="file_upload"></Input>
     <Button onclick={upload()}>Upload</Button>  
     <br></br>
-    <textarea id="json-result" style={{display:'none', height:'500px', width:'350px'}}></textarea>
+    <textarea id="json-result" style={{display:'none', height:'500px', width:'350px'}}></textarea>  */}
+    <div className="CoursesPanel">
+      <Form>
+        <Label htmlFor="upload">Upload File</Label>
+        <Input
+            type="file"
+            name="upload"
+            id="upload"
+            //onChange={readUploadFile}
+        />
+        <Button id="upload" name="upload" onClick={readUploadFile}>Upload</Button>
+      </Form>
+    </div>
     </>
   );
 }
 export default CoursesPanel;
+
+// function upload() {
+//   //alert("Upload clicked");
+//   var files = document.getElementById('file_upload').files;
+
+//   if(files.length==0){
+//     alert("Please choose any file...");
+//     return;
+//   }
+//   var filename = files[0].name;
+//   var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
+//   if (extension == '.XLS' || extension == '.XLSX') {
+//       excelFileToJSON(files[0]);
+//   }else{
+//       alert("Please select a valid excel file.");
+//   }
+// }
+// function excelFileToJSON(file){
+//   try {
+//     var reader = new FileReader();
+//     reader.readAsBinaryString(file);
+//     reader.onload = function(e) {
+
+//         var data = e.target.result;
+//         var workbook = XLSX.read(data, {
+//             type : 'binary'
+//         });
+//         var result = {};
+//         workbook.SheetNames.forEach(function(sheetName) {
+//             var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+//             if (roa.length > 0) {
+//                 result[sheetName] = roa;
+//             }
+//         });
+//         //displaying the json result
+//         var resultEle=document.getElementById("json-result");
+//         resultEle.value=JSON.stringify(result, null, 4);
+//         resultEle.style.display='block';
+//         }
+//     }catch(e){
+//         console.error(e);
+//     }
+// }
 
 
