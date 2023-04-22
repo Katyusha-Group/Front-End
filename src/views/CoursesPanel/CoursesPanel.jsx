9690e5e69feb22162d8e1f731cee48d55b00ceb6
Bahareh Kavousi <kavousi.b99@gmail.com>
2023-04-22 12:04:09 +0330

    Added "keyedTimetable" and "DayRow"

--- src/views/CoursesPanel/CoursesPanel.jsx
+++ src/views/CoursesPanel/CoursesPanel.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
-import classNames from "classnames";
+//import classNames from "classnames";
+import { useMemo } from 'react';
 // import { Line, Bar } from "react-chartjs-2";
 import {
   Button,
@@ -29,51 +30,65 @@ import dataJson from "../../assets/data/week.json"
 
 
 export default function CoursesPanel() {
-  let [data, setData] = React.useState(dataJson)
-  console.log(data)
-  const [bigChartData, setbigChartData] = React.useState("data1");
-  const setBgChartData = (name) => {
-    setbigChartData(name);
-  };
-  let defu = 20;
-  let length = 9.3;
-  let top_right = 10.8;
-  let top_defu = 13;
-  function lessons(){
-    return data.map((lesson) => {
-      return (
-        <div key={lesson.id} >
-          <div 
-            id = {lesson.id}
-            className="course text-center"
-            style={{
-              top: `${defu + length * lesson.day}%`,
-              right: `${top_defu + top_right * lesson.time}%`,
-              width: `${lesson.long == 1 ? 5 : 7.5}rem`,
-            }}
-          >
-            {lesson.name}
-          </div>
-        </div>
-      );
-    })
-  }
+  const timetable = [ // You did not show where you define this as you did not post your whole component, but you can leave it wherever it is currently.
+    {
+        "id" : 1,
+        "day" : "0",
+        "time" : "3",
+        "name" : "Math",
+        "teacher" : "X",
+        "room" : "A-1",
+    },
+    {
+        "id" : 1,
+        "day" : "0",
+        "time" : "1",
+        "name" : "Art",
+        "teacher" : "Y",
+        "room" : "B-3"
+    }
+  ]
+
+  const keyedTimetable = useMemo(() => {
+    const emptyDay = () => ({ 0: null, 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null })
+    return timetable.reduce(
+      (lessonsKeyedByDayAndPeriod, currentPeriod) => {
+        lessonsKeyedByDayAndPeriod[currentPeriod.day][currentPeriod.time] = currentPeriod
+        return lessonsKeyedByDayAndPeriod
+      },
+      {
+        0: emptyDay(),
+        1: emptyDay(),
+        2: emptyDay(),
+        3: emptyDay(),
+        4: emptyDay(),
+        5: emptyDay(),
+      }
+    )
+  }, [timetable])
+  const DayRow = ({ periods, dayName }) => (
+    <tr>
+      <td className="CoursesPanel_first_column text-center ">{dayName}</td>
+      {Object.entries(periods).map(([time, entry]) => {
+        return (
+          <td key={time}>
+            {entry !== null && (
+              <div>
+                {entry.name}
+                <br />
+                {entry.teacher}
+                <br />
+                {entry.room}
+              </div>
+            )}
+          </td>
+        )
+      })}
+    </tr>
+  )
   return (
     <>
-      {/* <div className="chart">{lessons()}</div> */}
-      {/* <Form>
-        <FormGroup>
-          <Label for="exampleSelect"></Label>
-          <Input className="select" type="select" name="select" id="exampleSelect">
-            <option>ریاضی</option>
-            <option>کامپایلر</option>
-            <option>فیزیک</option>
-            <option>ریزپر</option>
-          </Input>
-        </FormGroup>
-      </Form> */}
-      <Row>
-          
+      <Row>   
           <Col lg="12" sm="10">
             <Card>
               <CardHeader className="text-right">
@@ -92,173 +107,9 @@ export default function CoursesPanel() {
                       <th className="text-center ">۳ تا ۴:۳۰  </th>
                       <th className="text-center ">۴:۳۰ تا ۶  </th>
                       <th className="text-center ">۶ تا ۷:۳۰  </th>
-                      
                     </tr>
                   </thead>
-                  <tbody>
-                    <tr>
-                      <td className="CoursesPanel_first_column text-center  ">شنبه</td>
-                      <td>
-                        <div id="01"></div>
-                      </td>
-                      <td>
-                        <div id="02"></div>
-                      </td>
-                      <td>
-                        <div id="03"></div>
-                      </td>
-                      <td>
-                        <div id="04"></div>
-                      </td>
-                      <td>
-                        <div id="05"></div>
-                      </td>
-                      <td>
-                        <div id="06"></div>
-                      </td>
-                      <td>
-                        <div id="07"></div>
-                      </td>
-                      <td className="text-center">
-                        <div id="08"></div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="CoursesPanel_first_column text-center ">یکشنبه</td>
-                      <td>
-                        <div id="11"></div>
-                      </td>
-                      <td>
-                        <div id="12"></div>
-                      </td>
-                      <td>
-                        <div id="13"></div>
-                      </td>
-                      <td>
-                        <div id="14"></div>
-                      </td>
-                      <td>
-                        <div id="15"></div>
-                      </td>
-                      <td>
-                        <div id="16"></div>
-                      </td>
-                      <td>
-                        <div id="17"></div>
-                      </td>
-                      <td className="text-center">
-                        <div id="18"></div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="CoursesPanel_first_column text-center ">دوشنبه</td>
-                      <td>
-                        <div id="21"></div>
-                      </td>
-                      <td>
-                        <div id="22"></div>
-                      </td>
-                      <td>
-                        <div id="23"></div>
-                      </td>
-                      <td>
-                        <div id="24"></div>
-                      </td>
-                      <td>
-                        <div id="25"></div>
-                      </td>
-                      <td>
-                        <div id="26"></div>
-                      </td>
-                      <td>
-                        <div id="27"></div>
-                      </td>
-                      <td className="text-center">
-                        <div id="28"></div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="CoursesPanel_first_column text-center ">سه‌شنبه</td>
-                      <td>
-                        <div id="31"></div>
-                      </td>
-                      <td>
-                        <div id="32"></div>
-                      </td>
-                      <td>
-                        <div id="33"></div>
-                      </td>
-                      <td>
-                        <div id="34"></div>
-                      </td>
-                      <td>
-                        <div id="35"></div>
-                      </td>
-                      <td>
-                        <div id="36"></div>
-                      </td>
-                      <td>
-                        <div id="37"></div>
-                      </td>
-                      <td className="text-center">
-                        <div id="38"></div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="CoursesPanel_first_column text-center ">چهارشنبه</td>
-                      <td>
-                        <div id="41"></div>
-                      </td>
-                      <td>
-                        <div id="42"></div>
-                      </td>
-                      <td>
-                        <div id="43"></div>
-                      </td>
-                      <td>
-                        <div id="44"></div>
-                      </td>
-                      <td>
-                        <div id="45"></div>
-                      </td>
-                      <td>
-                        <div id="46"></div>
-                      </td>
-                      <td>
-                        <div id="47"></div>
-                      </td>
-                      <td className="text-center">
-                        <div id="48"></div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="CoursesPanel_first_column text-center ">پنج‌شنبه</td>
-                      <td>
-                        <div id="51"></div>
-                      </td>
-                      <td>
-                        <div id="52"></div>
-                      </td>
-                      <td>
-                        <div id="53"></div>
-                      </td>
-                      <td>
-                        <div id="54"></div>
-                      </td>
-                      <td>
-                        <div id="55"></div>
-                      </td>
-                      <td>
-                        <div id="56"></div>
-                      </td>
-                      <td>
-                        <div id="57"></div>
-                      </td>
-                      <td className="text-center">
-                        <div id="58"></div>
-                      </td>
-                    </tr>
-                  </tbody>
+                  
                 </Table>
               </CardBody>
             </Card>
