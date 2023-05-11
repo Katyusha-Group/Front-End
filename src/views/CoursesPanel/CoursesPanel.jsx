d6b3b7a4a752dc333af90b95d09fd12dff929ef4
Bahareh Kavousi <kavousi.b99@gmail.com>
2023-05-11 12:07:12 +0330

    Changing the functions
    in order to  work well with the new json type

--- src/views/CoursesPanel/CoursesPanel.jsx
+++ src/views/CoursesPanel/CoursesPanel.jsx
@@ -1,7 +1,6 @@
-
 import React from "react";
 import { useMemo } from 'react';
-
+import { useInfo } from "../../contexts/InfoContext";
 import {
   Card,
   CardHeader,
@@ -13,31 +12,109 @@ import {
   Button,
 } from "reactstrap";
 import "./CoursesPanel.css"
-
+import dataJson from "./Classes"
+// import { addNewLesson, changeInfo } from "../UserPage/UserPage"
 
 export default function CoursesPanel() {
-  let [timetable, settimetable] = React.useState([]);
-  const tokenJson = localStorage.getItem("authTokens");
-  const tokenClass = JSON.parse(tokenJson);
-  const token = tokenClass.token.access;
-  React.useEffect(() => {
-    fetch("https://katyushaiust.ir/allcourses-based-department/13", {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        settimetable(data);
-      })
-      .catch((error) => console.error(error));
-  }, []);
+  const { info, changeInfo } = useInfo();
+  let timetable = dataJson;
+  // let [timetable, settimetable] = React.useState([]);
+  // const tokenJson = localStorage.getItem("authTokens");
+  // const tokenClass = JSON.parse(tokenJson);
+  // const token = tokenClass.token.access;
+  // React.useEffect(() => {
+  //   fetch("https://katyushaiust.ir/allcourses-based-department/13", {
+  //     headers: { Authorization: `Bearer ${token}` },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       //console.log("DATA IS::::::: " + data);
+  //       settimetable(data);
+  //     })
+  //     .catch((error) => console.error(error));
+  // }, []);
   
+  // function addNewLesson(num) {
+  //   const tokenJson = localStorage.getItem("authTokens");
+  //   const tokenClass = JSON.parse(tokenJson);
+  //   // console.log("tokenClass", tokenClass);
+  //   const token = tokenClass.token.access;
+  //   // const response = await fetch("https://katyushaiust.ir/accounts/login/", {
+  //   //   method: "POST",
+  //   //   headers: {
+  //   //     "Content-Type": "application/json",
+  //   //   },
+  //   //   body: JSON.stringify({
+  //   //     username: formData.email,
+  //   //     password: formData.password,
+  //   //   }),
+  //   // });
+  //   // const data = await response.json();
+  //   // console.log(`num is : ${num}`);
+  //   // console.log(`type :`, typeof num);
+  
+  //   fetch("https://www.katyushaiust.ir/courses/my_courses/", {
+  //     method: "PUT",
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       complete_course_number: num,
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // console.log("gjgjhdsfffffhs post successfully");
+  //       console.log("put data", data);
+  //       // setData(data);
+  //     })
+  //     .catch((error) => console.error(error));
+  //   // console.log(data);
+  //   const activeRoute = (routeName) => {
+  //     return location.pathname === routeName ? "active" : "";
+  //   };
+  // }
+
+  function mapTimeToIndex (start_time) {
+    const times = [
+      "7:30:00",
+      "9:00:00",
+      "10:30:00",
+      "12:00:00",
+      "13:30:00",
+      "15:00:00",
+      "16:30:00",
+      "18:00:00",
+    ];
+    return times.findIndex(time => time === start_time);
+  }
+
+  function createCourseGroupsArray (courses)
+  {
+    const courseGroups = [];
+    for (let i=0; i<6; i++)
+    {
+      courseGroups[i] = Array(8).fill(0);
+    }
+    // courses.forEach(course => {
+    //   const day = parseInt(course.day, 10);
+    //   const timeIndex = mapTimeToIndex(course.time);
+    //   courseGroups[day][timeIndex]++;
+    // });
+    return courseGroups;
+  }
+
   const keyedTimetable = useMemo(() => {
     const emptySection = () => ({ 0: null, 1: null, 2: null, 3: null, 4: null, 5: null })
     const emptyDay = () => ({ 0: emptySection(), 1: emptySection(), 2: emptySection(), 3: emptySection(), 4: emptySection(), 5: emptySection(), 6: emptySection(), 7: emptySection() })
+    const NumInEachSlot = createCourseGroupsArray(timetable)
     return timetable.reduce(
       (lessonsKeyedByDayAndPeriod, currentPeriod) => {
-        lessonsKeyedByDayAndPeriod[currentPeriod.day][currentPeriod.time][currentPeriod.count] = currentPeriod
+        NumInEachSlot[currentPeriod.day][mapTimeToIndex(currentPeriod.time)]++;
+        let count = NumInEachSlot[currentPeriod.day][mapTimeToIndex(currentPeriod.time)];
+        lessonsKeyedByDayAndPeriod[currentPeriod.day][mapTimeToIndex(currentPeriod.time)][count] = currentPeriod
         return lessonsKeyedByDayAndPeriod
       },
       {
@@ -59,7 +136,14 @@ export default function CoursesPanel() {
                 <div className="Course" color="primary">
                   {entry.name} ({entry.class_gp})
                   <br/>
-                  <button className="btn-fill-AddCourseButton">+</button>
+                  <button className="btn-fill-AddCourseButton" onClick={() => {
+                      //console.log("x", x);
+                      //addNewLesson(x.complete_course_number);
+                      //changeInfo("courseChoosed", [...info.courseChoosed, x]);
+                      console.log("info", info.courseChoosed);
+                    }}>
+                      +
+                    </button>
                 </div>     
               )}     
             </div>
