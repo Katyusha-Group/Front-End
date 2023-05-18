be232069f2859de5d194266cf0a2f8571417e4eb
shakiba <shakiba.anarakifirooz@gmail.com>
2023-05-18 22:07:28 +0330

    added loading component

---
+++ src/components/LoadingAlert/LoadingAlert.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Swal from 'sweetalert2';
+
+
+export const showLoading =()=>{
+    Swal.fire({
+        title: 'کمی صبر کنید',
+        html: '... در حال بررسی درخواست  ',
+        allowOutsideClick: false,
+        timerProgressBar: true,
+        showConfirmButton: false,
+        background: '#3c3e5d',
+          color:'#ceccc0',
+        width:'25rem',
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading()
+        },
+      });
+}
+export const closeLoading = () =>{
+    Swal.close()
+}
+    
