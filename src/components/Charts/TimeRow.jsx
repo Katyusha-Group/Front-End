49ecfa9ff9211000beec5246dbd9ee95df1d1a68
Bahareh Kavousi <kavousi.b99@gmail.com>
2023-07-06 17:33:56 +0330

    Updating Hover

--- src/components/Charts/TimeRow.jsx
+++ src/components/Charts/TimeRow.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { showLoading } from "../../components/LoadingAlert/LoadingAlert";
 import { closeLoading } from "../../components/LoadingAlert/LoadingAlert";
+
 var weekday = new Array(7);
 weekday[0] = "شنبه";
 weekday[1] = "یکشنبه";
@@ -9,12 +10,27 @@ weekday[3] = "سه‌شنبه";
 weekday[4] = "چهارشنبه";
 weekday[5] = "پنج‌شنبه";
 weekday[6] = "جمعه";
+
 function timeStringToFloat(time) {
   var hoursMinutes = time.split(/[.:]/);
   var hours = parseInt(hoursMinutes[0], 10);
   var minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
   return hours + minutes / 60;
 }
+
+const [hoveredCourse, setHoveredCourse] = React.useState(null);
+const handleMouseOver = (event, entry) => {
+  document.getElementById(entry.complete_course_number + "x").style.display ="block";
+  setHoveredCourse(entry);
+  console.log("Hovered Course is: " + hoveredCourse.name)
+};
+
+const handleMouseOut = () => {
+  document.getElementById(entry.complete_course_number + "x").style.display ="none"
+  setHoveredCourse(null);
+};
+
+
 const DayPeriod = (Section) => (
     <div>
       {Object.entries(Section).map(([count, entry]) => {
@@ -25,20 +41,20 @@ const DayPeriod = (Section) => (
                 <div  className="ExamContainer">
                     <div 
                       id = {entry.complete_course_number}
-                      // className="Course"
                       className="exam text-center"
-                      onMouseOver={() =>
-                        (document.getElementById(entry.complete_course_number + "x").style.display =
-                          "block")
-                      }
-                      onMouseOut={() =>
-                        (document.getElementById(entry.complete_course_number + "x").style.display =
-                          "none")
-                      }
+                      onMouseOver={(e) => handleMouseOver(e, entry)}
+                      onMouseOut={handleMouseOut}
                     >
                         <div>
                           {/* {entry.name} */}
-                        <strong title= {entry.name}>{entry.name.length < 13 ? entry.name : entry.name.slice(0, 13) + "..."}</strong>
+                        {/* <strong title= {entry.name}>{entry.name.length < 13 ? entry.name : entry.name.slice(0, 13) + "..."}</strong> */}
+                        <strong title= {entry.name}>
+                            {
+                              hoveredCourse === entry ?
+                              entry.name :
+                              entry.name.length < 13 ? entry.name : entry.name.slice(0, 13) + "..."
+                            }
+                        </strong>
 
                         </div>
                         <div className="exam_hover" id={entry.complete_course_number + "x"}>
@@ -82,3 +98,69 @@ const TimeRow = ({ periods, ExamT }) => {
     );
 };
 export default TimeRow;
+
+
+
+
+
+
+// import React, { useState } from 'react';
+
+// function DayPeriod(Section) {
+//   const [hoveredCourse, setHoveredCourse] = useState(null);
+
+//   const handleMouseOver = (event, entry) => {
+//     setHoveredCourse(entry);
+//   };
+
+//   const handleMouseOut = () => {
+//     setHoveredCourse(null);
+//   };
+
+//   return (
+//     <div>
+//       {Object.entries(Section).map(([count, entry]) => {
+//         return (
+//           <div className="ExamListContainer">
+//             {entry !== null && (
+//               <div key={entry.complete_course_number}>
+//                 <div className="ExamContainer">
+//                   <div 
+//                     id={entry.complete_course_number}
+//                     className="exam text-center"
+//                     onMouseOver={(e) => handleMouseOver(e, entry)}
+//                     onMouseOut={handleMouseOut}
+//                   >
+//                     <div>
+//                       <strong title={entry.name}>
+//                         {entry.name.length < 13 ? entry.name : entry.name.slice(0, 13) + "..."}
+//                       </strong>
+//                     </div>
+//                     <div className="exam_hover" id={entry.complete_course_number + "x"}>
+//                       <div className="dir-left">
+//                         {entry.complete_course_number}
+//                       </div>
+//                       <div>
+//                         {weekday[parseInt(entry.course_times[0].course_day)]}
+//                       </div>
+//                       <div>
+//                         {timeStringToFloat(entry.exam_times[0].exam_start_time) +
+//                           "-" +
+//                           timeStringToFloat(entry.exam_times[0].exam_end_time)}
+//                       </div>
+//                       {hoveredCourse === entry && (
+//                         <div>
+//                           {entry.name}
+//                         </div>
+//                       )}
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             )}
+//           </div>
+//         )
+//       })}
+//     </div>
+//   );
+// }
