27d56133fff8aad27474ea7aebc3f32a8b373766
Bahareh Kavousi <kavousi.b99@gmail.com>
2023-07-07 13:02:52 +0330

    Added Animation :)

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -5,7 +5,8 @@ import {Link} from "react-router-dom";
 import { useInfo } from "../contexts/InfoContext.jsx";
 import styles from "../assets/css/LandingPage.css";
 // import BGImage from "../assets/img/Landing.jpg";
-import BGImage from "../assets/img/Landing page-amico.svg";
+import lottie from 'lottie-web';
+// import BGImage from "../assets/img/28893-book-loading.json";
 
 import {
   Button,
@@ -16,19 +17,39 @@ import {
 } from "reactstrap";
 
 
-var sectionStyle = {
-  width: "100%",
-  height: "400px",
-  backgroundImage: `url(${BGImage})`
-};
+// var sectionStyle = {
+//   width: "100%",
+//   height: "400px",
+//   backgroundImage: `url(${BGImage})`
+// };
+
 
 function LandingPage() {
+  React.useEffect(() => {
+    const animation = lottie.loadAnimation({
+      container: document.getElementById('animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      // path: "../src/assets/img/28893-book-loading.json"
+      // path: "../src/assets/img/92377-quiz-mode.json"
+      // path: "../src/assets/img/133116-education-edit.json"
+      // path: "../src/assets/img/132921-education-services.json"
+      // path: "../src/assets/img/74130-education.json"
+      path: "../src/assets/img/88025-smart-owl-education-animation-icons.json"
+    });
+
+    return () => {
+      animation.destroy();
+    };
+  }, []);
   return (
     <>
     <Container>
     <Row>
       <Col className="BGimgCol">
-        <img className="BGimg" src={BGImage}/>
+        {/* <img className="BGimg" src={BGImage}/> */}
+        <div id="animation"></div>
       </Col>
       <Col className="LandingPageCardCol">
         <Card className="LandingPageCard">
@@ -39,18 +60,18 @@ function LandingPage() {
               </div>
               <br/>
               <div className="div-3">
-                این سایت به شما کمک می کنه که:
+                این سامانه به شما کمک می کنه که:
                 <li>
                   برنامه هفتگی و لیست واحدها و امتحاناتون رو ببینین 📆
                 </li>
+                {/* <li>
+                  شرایط دروس در ترم های گذشته رو بررسی کنین
+                </li> */}
                 <li>
-                   📒
+                 برای ترم آینده برنامه ریزی کنین 📒
                 </li>
                 <li>
-                  برای ترم آینده برنامه ریزی کنین 
-                </li>
-                <li>
-                    انتخاب واحد بهتری داشته باشین 
+                  و کلی قابلیت خوب دیگه برای اینکه بتونین انتخاب واحد خوب و راحتی داشته باشین
                 </li>
               </div>
               <br/>
@@ -71,7 +92,7 @@ function LandingPage() {
           <span>
             <div className="div-4">
               <Row>
-                در صورت وجود هرگونه مشکل با ما در ارتباط باشید:
+                کنجکاویم که نظرات و پیشنهاداتتون رو درباره کاتیوشا بدونیم:
                 katyushaiust@gmail.com
               </Row>
             </div>
