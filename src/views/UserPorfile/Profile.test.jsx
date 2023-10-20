a22c20403aed0df76286db31024e98549f5f52c4
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2023-10-20 12:00:25 +0330

    test/ write test for profile page
    just render component because there is no function

---
+++ src/views/UserPorfile/Profile.test.jsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import Profile from './Profile';
+import { test,it } from 'vitest';
+
+test('Profile component', () => {
+  it('should render the ProfileHeader component', () => {
+    const { getByTestId } = render(<Profile />);
+    expect(getByTestId('profile-header')).toBeInTheDocument();
+  });
+
+  it('should render the Instructorall component', () => {
+    const { getByTestId } = render(<Profile />);
+    expect(getByTestId('instructor-all')).toBeInTheDocument();
+  });
+
+  it('should render the weekly schedule table', () => {
+    const { getByText } = render(<Profile />);
+    expect(getByText('برنامه هفتگی')).toBeInTheDocument();
+  });
+});
