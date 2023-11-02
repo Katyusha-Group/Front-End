cd8e549ebd18e4a1ebb420ac660cbed0b2268b59
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2023-11-02 22:10:23 +0330

    refactor(CoursesPanel): Modular CSS

--- src/views/CoursesPanel/DayRow.jsx
+++ src/views/CoursesPanel/DayRow.jsx
@@ -1,13 +1,13 @@
 import DayPeriod from "./DayPeriod";
-
+import * as style from  "./CoursesPanel.module.css";
 const DayRow = ({ periods, dayName }) => {
     return (
     <>
-      <tr className="TableROW">
-      <td className="CoursesPanel_column text-center">{dayName}</td>
+      <tr className={style.TableROW}>
+      <td className={`${style.CoursesPanel_column} text-center`}>{dayName}</td>
       {Object.entries(periods).map(([time, entry]) => {
           return (
-          <td className="CoursesPanel_column2" key={time}>
+          <td className={style.CoursesPanel_column2} key={time}>
               {DayPeriod(entry)}
           </td>
           )
