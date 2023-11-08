99ced845902780c10054b71ada4a263d6ae2780f
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-11-09 00:38:49 +0330

    ref(timeline): refactor css and code
    import colors in css files and js code

--- src/views/TimeLine/Timeline.jsx
+++ src/views/TimeLine/Timeline.jsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import Tweet from './Tweet';
-import styles from '../../assets/css/Timeline/Timeline.module.css';
-import { useState } from 'react';
-
+import React from "react";
+import Tweet from "./Tweet";
+import styles from "../../assets/css/Timeline/Timeline.module.css";
+import { useState } from "react";
+import { Card } from "reactstrap";
 
 const tweets = [
-  { id: 1, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White',name: 'جت وایت' },
-  { id: 2, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett',name: 'وایت جت' },
-  // Add more tweets as needed
-  { id: 3, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White',name: 'جت وایت' },
-  { id: 4, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett',name: 'وایت جت' },
-  // Add more tweets as needed
-  { id: 5, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White',name: 'جت وایت' },
-  { id: 6, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett',name: 'وایت جت' },
-  // Add more tweets as needed
-  { id: 7, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White',name: 'جت وایت' },
-  { id: 8, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett',name: 'وایت جت' },
-  // Add more tweets as needed
+  {
+    id: 1,
+    text: "عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.",
+    username: "jett White",
+    name: "جت وایت",
+  },
 ];
 
 function Timeline() {
-  const [activeTab, setActiveTab] = useState('tweets');
+  const [activeTab, setActiveTab] = useState("tweets");
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
@@ -28,38 +22,37 @@ function Timeline() {
 
   return (
     <>
-    <div className={styles.timeline}>
-      <div className={styles.tabs}>
-        <button
-          className={activeTab === 'tweets' ? styles.activeTab : styles.tab}
-          onClick={() => handleTabClick('tweets')}
-        >
-          دنبال کننده ها
-        </button>
-        <button
-          className={activeTab === 'media' ? styles.activeTab : styles.tab}
-          onClick={() => handleTabClick('media')}
-        >
-          برای شما
-        </button>
-      </div>
-      <div className={styles.content}>
-        {activeTab === 'tweets' && (
-          <div className={styles.tweetsContainer}>
-            {tweets.map((tweet) => (
-              <Tweet key={tweet.id} tweet={tweet} />
-            ))}
-          </div>
-        )}
-        {activeTab === 'media' && (
-          <div className={styles.tweetsContainer}>
-            {/* Render media content here */}
-          </div>
-        )}
-      </div>
-    </div>
+      <Card className={styles.timeline}>
+        <div className={styles.tabs}>
+          <button
+            className={activeTab === "tweets" ? styles.activeTab : styles.tab}
+            onClick={() => handleTabClick("tweets")}
+          >
+            دنبال کننده ها
+          </button>
+          <button
+            className={activeTab === "media" ? styles.activeTab : styles.tab}
+            onClick={() => handleTabClick("media")}
+          >
+            برای شما
+          </button>
+        </div>
+        <div className={styles.content}>
+          {activeTab === "tweets" && (
+            <div className={styles.tweetsContainer}>
+              {tweets.map((tweet) => (
+                <Tweet key={tweet.id} tweet={tweet} />
+              ))}
+            </div>
+          )}
+          {activeTab === "media" && (
+            <div className={styles.tweetsContainer}>
+            </div>
+          )}
+        </div>
+      </Card>
     </>
   );
-  }
+}
 
 export default Timeline;
