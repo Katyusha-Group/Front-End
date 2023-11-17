41ea69242dfb07897ddd74a1236a9ec7005e1e27
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-11-17 19:40:52 +0330

    Fix(Timeline): Timeline had conflict with Timeline_Profile
    fix: timeline had conflict with profile

--- src/views/TimeLine/Timeline_Profile.jsx
+++ src/views/TimeLine/Timeline_Profile.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Tweet from './Tweet';
-import styles from '../../assets/css/Timeline/Timeline_Profile.module.css';
+import React from "react";
+import Tweet from "./Tweet";
+import styles from "../../assets/css/Timeline/Timeline_Profile.module.css";
 import * as style from "../../components/ModalLessons/ModalLesson.module.css";
-import { useState } from 'react';
-import { GETTweets } from '../../hooks/GETTweets';
+import { useState } from "react";
+import { GETTweets } from "../../hooks/GETTweets";
 import {
   dayOfWeek,
   timeStringToFloat,
@@ -20,77 +20,73 @@ import {
   Col,
   Label,
 } from "reactstrap";
+import { useTweets } from "../../hooks/Twitter/useTweets";
 const Tweets = [
-  { id: 1, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White', name: 'جت وایت' },
-  { id: 2, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett', name: 'وایت جت' },
   // Add more tweets as needed
-  { id: 3, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White', name: 'جت وایت' },
-  { id: 4, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett', name: 'وایت جت' },
-  // Add more tweets as needed
-  { id: 5, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White', name: 'جت وایت' },
-  { id: 6, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett', name: 'وایت جت' },
-  // Add more tweets as needed
-  { id: 7, text: 'عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.عرفان بهترین صاحبیه که تو زندگیم داشتم. بهترین غذا هارو بهم می ده. دلم می خواد لیسش بزنم.', username: 'jett White', name: 'جت وایت' },
-  { id: 8, text: 'من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی', username: 'white Jett', name: 'وایت جت' },
+  {
+    id: 8,
+    text: "من یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشیمن یه پرندم آرزو دارم ت .یارم باشی",
+    username: "white Jett",
+    profile: { name: "وایت جت" },
+  },
   // Add more tweets as needed
 ];
 const x = {
-  "complete_course_number": "2211299_03",
-  "name": "ریز پردازنده و زبان اسمبلی",
-  "group_number": "03",
-  "total_unit": 3,
-  "practical_unit": 0,
-  "capacity": 50,
-  "registered_count": 36,
-  "waiting_count": 0,
-  "sex": "B",
-  "emergency_deletion": true,
-  "registration_limit": "مجاز برای مقطع کارشناسی، دانشکده مهندسی کامپیوتر، گروه آموزشی واحد آموزش کارشناسی، رشته مهندسی کامپیوتر،",
-  "description": "nan",
-  "presentation_type": "N",
-  "teachers": [
+  complete_course_number: "2211299_03",
+  name: "ریز پردازنده و زبان اسمبلی",
+  group_number: "03",
+  total_unit: 3,
+  practical_unit: 0,
+  capacity: 50,
+  registered_count: 36,
+  waiting_count: 0,
+  sex: "B",
+  emergency_deletion: true,
+  registration_limit:
+    "مجاز برای مقطع کارشناسی، دانشکده مهندسی کامپیوتر، گروه آموزشی واحد آموزش کارشناسی، رشته مهندسی کامپیوتر،",
+  description: "nan",
+  presentation_type: "N",
+  teachers: [
     {
-      "id": 3609,
-      "name": "هاشم مشحون",
-      "teacher_image": "http://37.32.13.62/var/www/media/images/teachers_image/13_Y6rbyvu.png"
-    }
+      id: 3609,
+      name: "هاشم مشحون",
+      teacher_image:
+        "http://37.32.13.62/var/www/media/images/teachers_image/13_Y6rbyvu.png",
+    },
   ],
-  "exam_times": [
+  exam_times: [
     {
-      "date": "1402-11-04",
-      "exam_start_time": "14:00:00",
-      "exam_end_time": "16:00:00"
-    }
+      date: "1402-11-04",
+      exam_start_time: "14:00:00",
+      exam_end_time: "16:00:00",
+    },
   ],
-  "course_times": [
+  course_times: [
     {
-      "course_day": "1",
-      "course_start_time": "14:30:00",
-      "course_end_time": "16:00:00",
-      "place": "دانشکده کامپیوتر کلاس شماره 116",
-      "course_time_representation": 7
+      course_day: "1",
+      course_start_time: "14:30:00",
+      course_end_time: "16:00:00",
+      place: "دانشکده کامپیوتر کلاس شماره 116",
+      course_time_representation: 7,
     },
     {
-      "course_day": "3",
-      "course_start_time": "14:30:00",
-      "course_end_time": "16:00:00",
-      "place": "دانشکده کامپیوتر کلاس شماره 116",
-      "course_time_representation": 7
-    }
+      course_day: "3",
+      course_start_time: "14:30:00",
+      course_end_time: "16:00:00",
+      place: "دانشکده کامپیوتر کلاس شماره 116",
+      course_time_representation: 7,
+    },
   ],
-  "is_allowed": true,
-  "added_to_calendar_count": 1
-}
+  is_allowed: true,
+  added_to_calendar_count: 1,
+};
 let tabsList = [
   ["tweets", "دنبال کننده ها"],
-  ["media", "برای شما"]
+  ["media", "برای شما"],
 ];
 
 function Timeline({ tabsList }) {
-  // console.log("Tabs are: " + tabsList);
-  const [activeTab, setActiveTab] = useState('Main');
-  // const {Tweets, setTweets, loading} = GETTweets();
-  // setTweets(tweets);
+  const [activeTab, setActiveTab] = useState("Main");
   const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
@@ -98,38 +94,17 @@ function Timeline({ tabsList }) {
   if (tabsList == undefined) {
     tabsList = [
       ["Tweets", "دنبال کننده ها"],
-      ["media", "برای شما"]
+      ["media", "برای شما"],
     ];
   }
+
+  const { data: tweets, setData: setTweets, loading } = useTweets("get", true);
+  const [open, setOpen] = useState(false);
 
   return (
     <>
       <div className={styles.timeline}>
         <div className={styles.tabs}>
-          {/* <button
-          className={activeTab === 'Main' ? styles.activeTab : styles.tab}
-          onClick={() => handleTabClick('Main')}
-        >
-          صفحه اصلی
-        </button>
-        <button
-          className={activeTab === 'Tweets' ? styles.activeTab : styles.tab}
-          onClick={() => handleTabClick('Tweets')}
-        >
-          پست ها
-        </button>
-        <button
-          className={activeTab === 'Likes' ? styles.activeTab : styles.tab}
-          onClick={() => handleTabClick('Likes')}
-        >
-          Likes
-        </button>
-        <button
-          className={activeTab === 'Comments' ? styles.activeTab : styles.tab}
-          onClick={() => handleTabClick('Comments')}
-        >
-          Comments
-        </button> */}
           {tabsList.map((entry, index) => (
             <button
               key={index}
@@ -138,19 +113,21 @@ function Timeline({ tabsList }) {
             >
               {entry[1]}
             </button>
-          ))
-          }
+          ))}
         </div>
 
-        {activeTab === 'Tweets' && (
+        {activeTab === "Tweets" && (
           <div className={styles.tweetsContainer}>
-            {Tweets.map((tweet) => (
-              <Tweet key={tweet.id} tweet={tweet} />
+            {tweets.map((tweet) => (
+              <Tweet key={tweet.id}
+              tweet={tweet}
+              setOpenComment={setOpen}
+              setTweets={setTweets}/>
             ))}
           </div>
         )}
         <div className={styles.content}>
-          {activeTab === 'Main' && (
+          {activeTab === "Main" && (
             <div className={styles.tweetsContainer}>
               {/* Render media content here */}
               <div>
@@ -158,24 +135,26 @@ function Timeline({ tabsList }) {
                   <Col md="5">
                     <Card className={style.ModalLessonDataCard1}>
                       <Row>
-                        <Col md="4"
-                          className="p-0">
+                        <Col md="4" className="p-0">
                           <img
                             className={style.ModalProfessorImage}
                             src={x.teachers[0].teacher_image}
                             alt="professorImage"
                           />
                         </Col>
-                        <Col md="8"
-                          className="text-right p-0 mr-0"
-                        >
-                          <p className={style.courseTitle}>استاد&nbsp;
+                        <Col md="8" className="text-right p-0 mr-0">
+                          <p className={style.courseTitle}>
+                            استاد&nbsp;
                             <span className={style.courseText}>
-                              {x.teachers.map((y) => y.name).join(" , ")}</span>
+                              {x.teachers.map((y) => y.name).join(" , ")}
+                            </span>
                           </p>
 
-                          <p className={`${style.courseTitle}`}
-                            style={{ display: "flex" }}>کد درس&nbsp;
+                          <p
+                            className={`${style.courseTitle}`}
+                            style={{ display: "flex" }}
+                          >
+                            کد درس&nbsp;
                             <span
                               className={style.courseText}
                               style={{ direction: "ltr" }}
@@ -195,7 +174,9 @@ function Timeline({ tabsList }) {
 
                     <Card className={style.ModalLessonDataCard2}>
                       <Col className="text-right">
-                        <p className={style.courseTitleNotInline}>زمان برگزاری</p>
+                        <p className={style.courseTitleNotInline}>
+                          زمان برگزاری
+                        </p>
                         <span className={style.courseText}>
                           {x.course_times.map((t) => (
                             <text>{dayOfWeek(t.course_day)} </text>
@@ -204,8 +185,7 @@ function Timeline({ tabsList }) {
                           {" / "}
                         </span>
                         <text className={style.courseText}>
-                          {convertTime(x.course_times[0].course_start_time)}{" "}
-                          تا{" "}
+                          {convertTime(x.course_times[0].course_start_time)} تا{" "}
                           {convertTime(x.course_times[0].course_end_time)}
                         </text>
                       </Col>
@@ -217,33 +197,34 @@ function Timeline({ tabsList }) {
                           <text dir="ltr" className={style.courseText}>
                             {"تاریخ"}
                           </text>{" "}
-                          <text className={style.courseText}
-                            style={{ direction: "ltr" }}>
+                          <text
+                            className={style.courseText}
+                            style={{ direction: "ltr" }}
+                          >
                             {x.exam_times[0].date}
                           </text>
                           <text className={style.courseText}>
                             {" / "}
                             {convertTime(
                               x.exam_times[0].exam_start_time
-                            )}{" "}
-                            تا {convertTime(x.exam_times[0].exam_end_time)}
+                            )} تا {convertTime(x.exam_times[0].exam_end_time)}
                           </text>
                         </Col>
                       )}
                     </Card>
                   </Col>
                   <Col md="7">
-                    <Card className={`${style.ModalLessonDataCard3} text-right`}>
+                    <Card
+                      className={`${style.ModalLessonDataCard3} text-right`}
+                    >
                       <p className={style.courseTitle}>
-                        ثبت نام
-                        شده&nbsp;
+                        ثبت نام شده&nbsp;
                         <text className={style.courseText}>
                           {x.registered_count} از {x.capacity}
                         </text>
                       </p>
                       <p className={style.courseTitle}>
-                        تعداد در صف
-                        انتظار&nbsp;
+                        تعداد در صف انتظار&nbsp;
                         <text className={style.courseText}>
                           {x.waiting_count} {"نفر"}
                         </text>
@@ -257,7 +238,8 @@ function Timeline({ tabsList }) {
                       <p className={style.courseTitle}>
                         تعداد کل واحد ها&nbsp;
                         <text className={style.courseText}>
-                          {x.total_unit} {"واحد"}</text>
+                          {x.total_unit} {"واحد"}
+                        </text>
                       </p>
                       <p className={style.courseTitle}>
                         تعداد واحد های عملی&nbsp;
@@ -266,8 +248,7 @@ function Timeline({ tabsList }) {
                         </text>
                       </p>
                       <p className={style.courseTitle}>
-                        قابل اخذ بودن این درس برای
-                        شما&nbsp;
+                        قابل اخذ بودن این درس برای شما&nbsp;
                         <text className={style.courseText}>
                           {x.is_allowed ? "بله" : "خیر"}
                         </text>
@@ -291,17 +272,27 @@ function Timeline({ tabsList }) {
               </div>
             </div>
           )}
-          {activeTab === 'Likes' && (
+          {activeTab === "Likes" && (
             <div className={styles.tweetsContainer}>
-              {Tweets.map((tweet) => (
-                <Tweet key={tweet.id} tweet={tweet} />
+              {tweets.map((tweet) => (
+                <Tweet
+                  key={tweet.id}
+                  tweet={tweet}
+                  setOpenComment={setOpen}
+                  setTweets={setTweets}
+                />
               ))}
             </div>
           )}
-          {activeTab === 'Comments' && (
+          {activeTab === "Comments" && (
             <div className={styles.tweetsContainer}>
-              {Tweets.map((tweet) => (
-                <Tweet key={tweet.id} tweet={tweet} />
+              {tweets.map((tweet) => (
+                <Tweet
+                  key={tweet.id}
+                  tweet={tweet}
+                  setOpenComment={setOpen}
+                  setTweets={setTweets}
+                />
               ))}
             </div>
           )}
