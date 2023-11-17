a198f68ec52f0c74bae5c8209ba0dbb085ced43e
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2023-11-17 07:00:48 +0330

    fix(Profile): Fixed Followings and Followers lists

--- src/views/UserPorfile/Instructorall.jsx
+++ src/views/UserPorfile/Instructorall.jsx
@@ -27,31 +27,38 @@ function Instructorall({ username, IsFollowing, IsModal, Followings, Followers }
     //     },  
     // ];
     // console.log("Fetched followings: " + Followings);
-    const Following_Follower = !IsFollowing ? "دنبال شونده"  : "دنبال کننده";
-    console.log("Followers in InstructorAll: " , Followers);
+    const Title = IsFollowing ? "دنبال شونده"  : "دنبال کننده";
+    // const Following_Follower = IsFollowing ? "Following" : "Follower";
+    // console.log("Followers in InstructorAll: " , Followers);
     return (
         <div>
-            {/* <p>{IsModal}</p> */}
-            {
-
-                IsFollowing || !IsModal ?
-
-                    Followings && Followings.length > 0 ? 
-                    Followings.map ( (entry, index) => (
-                        <Instructor key={index} User={entry}/>
-                    )) :
-                    <p>هیچ {Following_Follower} ای یافت نشد</p>
-
-                    :
-
-                    Followers && Followers.length > 0 ? 
-                    Followers.map ( (entry, index) => (
-                        <Instructor key={index} User={entry}/>
-                    )) :
-                    <p>هیچ {Following_Follower} ای یافت نشد</p>
-            }
+            {IsModal ? (
+                Following_Component(Followings, Title)
+            ) : (
+                IsFollowing ? Following_Component(Followings, Title) : Follower_Component(Followers, Title)
+            )}
         </div>
     );
 }
 
 export default Instructorall;
+
+function Following_Component (Followings, Title) {
+    return (
+        Followings && Followings.length > 0 ? 
+        Followings.map ( (entry, index) => (
+            <Instructor key={index} User={entry}/>
+        )) :
+        <p>هیج {Title} ای یافت نشد</p>
+    )
+}
+
+function Follower_Component (Followers, Title) {
+    return (
+        Followers && Followers.length > 0 ? 
+        Followers.map ( (entry, index) => (
+            <Instructor key={index} User={entry}/>
+        )) :
+        <p>هیج {Title} ای یافت نشد</p>   
+    )
+}
