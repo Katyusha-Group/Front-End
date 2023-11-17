42d9d881212b9b68ac28d962c2c5f2196cb7c325
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-11-17 14:59:56 +0330

    FIx(timeline): fix style of searchbar and Sidebar
    Sidebar and search bar fix change some style

--- src/views/Searchbar.jsx
+++ src/views/Searchbar.jsx
@@ -1,35 +1,32 @@
-import React, { useState ,useEffect} from 'react';
-import styles from '../assets/css/Searchbar.module.css';
-import Searchfield from './Searchfield';
-import UserSearchResponce from './UserSearchResponce';
-import { Card } from 'reactstrap';
-import { useAllProfiles } from '../hooks/useSearchprofile';
-import axios from 'axios';
-import { set } from 'lodash';
-import { use } from 'chai';
+import React, { useState, useEffect } from "react";
+import styles from "../assets/css/Searchbar.module.css";
+import Searchfield from "./Searchfield";
+import UserSearchResponce from "./UserSearchResponce";
+import { Card } from "reactstrap";
+import { useAllProfiles } from "../hooks/useSearchprofile";
+import axios from "axios";
+import { set } from "lodash";
+import { use } from "chai";
 
 const Searchbar = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { filteredProfiles, loading } = useAllProfiles(searchQuery);
-  useEffect(() => {
-  }, [searchQuery]);
+  useEffect(() => {}, [searchQuery]);
 
-  
   return (
     <Card className={styles.main}>
       <Searchfield setSearchQuery={setSearchQuery} />
 
-      
-          {loading ? (
-            <div></div>
-          ) : (
-            filteredProfiles.map((item, index) => {
-              return <UserSearchResponce res={item} />;
-            })
-          )         
-          }
-
+      <div className={styles.searchBox}>
+        {loading ? (
+          <div></div>
+        ) : (
+          filteredProfiles.map((item, index) => {
+            return <UserSearchResponce res={item} key={index} />;
+          })
+        )}
+      </div>
     </Card>
   );
 };
