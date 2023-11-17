ae5172adf61fbeba6fb02a93820055c61656b58a
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2023-11-17 14:58:16 +0330

    feat(Profile): Adding timeline

--- src/views/TimeLine/Timeline.jsx
+++ src/views/TimeLine/Timeline.jsx
@@ -84,6 +84,145 @@ function Timeline({tabsList}) {
         {activeTab === 'Main' && (
           <div className={styles.tweetsContainer}>
             {/* Render media content here */}
+            <div
+                style={{
+                  display: timelineData == "data0" ? "block" : "none",
+                }}>
+                <Row>
+                  <Col md="5">
+                    <Card className={style.ModalLessonDataCard1}>
+                      <Row>
+                        <Col md="4"
+                          className="p-0">
+                          <img
+                            className={style.ModalProfessorImage}
+                            src={x.teachers[0].teacher_image}
+                            alt="professorImage"
+                          />
+                        </Col>
+                        <Col md="8"
+                          className="text-right p-0 mr-0"
+                        >
+                          <p className={style.courseTitle}>استاد&nbsp;
+                            <span className={style.courseText}>
+                              {x.teachers.map((y) => y.name).join(" , ")}</span>
+                          </p>
+
+                          <p className={`${style.courseTitle}`}
+                            style={{ display: "flex" }}>کد درس&nbsp;
+                            <span
+                              className={style.courseText}
+                              style={{ direction: "ltr" }}
+                            >
+                              {x.complete_course_number}
+                            </span>
+                          </p>
+                          <p className={style.courseTitle}>
+                            جنسیت&nbsp;
+                            <span className={style.courseText}>
+                              {sexTostring(x.sex)}
+                            </span>
+                          </p>
+                        </Col>
+                      </Row>
+                    </Card>
+
+                    <Card className={style.ModalLessonDataCard2}>
+                      <Col className="text-right">
+                        <p className={style.courseTitleNotInline}>زمان برگزاری</p>
+                        <span className={style.courseText}>
+                          {x.course_times.map((t) => (
+                            <text>{dayOfWeek(t.course_day)} </text>
+                          ))}
+
+                          {" / "}
+                        </span>
+                        <text className={style.courseText}>
+                          {convertTime(x.course_times[0].course_start_time)}{" "}
+                          تا{" "}
+                          {convertTime(x.course_times[0].course_end_time)}
+                        </text>
+                      </Col>
+                      {x.exam_times.length === 0 ? null : (
+                        <Col className="text-right">
+                          <p className={style.courseTitleNotInline}>
+                            زمان آزمون پایانی
+                          </p>
+                          <text dir="ltr" className={style.courseText}>
+                            {"تاریخ"}
+                          </text>{" "}
+                          <text className={style.courseText}
+                            style={{ direction: "ltr" }}>
+                            {x.exam_times[0].date}
+                          </text>
+                          <text className={style.courseText}>
+                            {" / "}
+                            {convertTime(
+                              x.exam_times[0].exam_start_time
+                            )}{" "}
+                            تا {convertTime(x.exam_times[0].exam_end_time)}
+                          </text>
+                        </Col>
+                      )}
+                    </Card>
+                  </Col>
+                  <Col md="7">
+                    <Card className={`${style.ModalLessonDataCard3} text-right`}>
+                      <p className={style.courseTitle}>
+                        ثبت نام
+                        شده&nbsp;
+                        <text className={style.courseText}>
+                          {x.registered_count} از {x.capacity}
+                        </text>
+                      </p>
+                      <p className={style.courseTitle}>
+                        تعداد در صف
+                        انتظار&nbsp;
+                        <text className={style.courseText}>
+                          {x.waiting_count} {"نفر"}
+                        </text>
+                      </p>
+                      <p className={style.courseTitle}>
+                        تعداد اخذ شده در کاتیوشا&nbsp;
+                        <text className={style.courseText}>
+                          {x.added_to_calendar_count} {"نفر"}
+                        </text>
+                      </p>
+                      <p className={style.courseTitle}>
+                        تعداد کل واحد ها&nbsp;
+                        <text className={style.courseText}>
+                          {x.total_unit} {"واحد"}</text>
+                      </p>
+                      <p className={style.courseTitle}>
+                        تعداد واحد های عملی&nbsp;
+                        <text className={style.courseText}>
+                          {x.practical_unit}&nbsp;{"واحد"}
+                        </text>
+                      </p>
+                      <p className={style.courseTitle}>
+                        قابل اخذ بودن این درس برای
+                        شما&nbsp;
+                        <text className={style.courseText}>
+                          {x.is_allowed ? "بله" : "خیر"}
+                        </text>
+                      </p>
+
+                      {x.description === "nan" ? null : (
+                        <Row>
+                          <Col className="text-right" md="10">
+                            <p className={style.courseTitle}>
+                              توضیحات&nbsp;
+                              <text className={style.courseText}>
+                                {x.description}
+                              </text>
+                            </p>
+                          </Col>
+                        </Row>
+                      )}
+                    </Card>
+                  </Col>
+                </Row>
+              </div>
           </div>
         )}
         {activeTab === 'Tweets' && (
