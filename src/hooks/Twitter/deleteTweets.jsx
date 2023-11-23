e027f6cfefe11a3a930664d09a02c78a05fc54f9
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-11-23 09:48:59 +0330

    Feat(timeline): add drop down to comments
    add drop down to delete and block user

---
+++ src/hooks/Twitter/deleteTweets.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import { apis } from "../../assets/apis";
+
+export const deleteTweet = async (id) => {
+  const token = JSON.parse(localStorage.getItem("authTokens")).token.access;
+  let data = "";
+  let config = {
+    method: "delete",
+    maxBodyLength: Infinity,
+    url: apis["tweets"]["tweets"]+id,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    data:data
+  };
+
+  try {
+    const response = await axios.request(config);
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/views/TimeLine/Tweet.jsx
+++ src/views/TimeLine/Tweet.jsx
@@ -1,16 +1,50 @@
 import React from "react";
 import styles from "../../assets/css/Timeline/Tweet.module.css";
 import { Button, ButtonGroup, Card } from "reactstrap";
-import CommentModal from "../../components/Modal/Comments"
+import CommentModal from "../../components/Modal/Comments";
 import { likes } from "../../hooks/Twitter/likes";
 import { useState } from "react";
-import { likes } from "../../hooks/Twitter/likes";
-function Tweet({ tweet, setOpenComment,setTweets }) {
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+import { deleteTweet } from "../../hooks/Twitter/deleteTweets";
+function Tweet({ tweet, setOpenComment, setTweets, direction, ...args }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
   const [open, setOpen] = useState(false);
-  const [like, setLike] = useState(false)
+  const [like, setLike] = useState(false);
   return (
     <>
-      <Card className={styles.tweet}>
+      <Card className={styles.tweet} >
+        <Dropdown isOpen={dropdownOpen} className={styles.moreInfo} toggle={toggle} direction={direction} size="sm">
+          <DropdownToggle style={{background:"initial",}} caret>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 96 96"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_135_5)">
+                <path
+                  d="M24 40C19.6 40 16 43.6 16 48C16 52.4 19.6 56 24 56C28.4 56 32 52.4 32 48C32 43.6 28.4 40 24 40ZM72 40C67.6 40 64 43.6 64 48C64 52.4 67.6 56 72 56C76.4 56 80 52.4 80 48C80 43.6 76.4 40 72 40ZM48 40C43.6 40 40 43.6 40 48C40 52.4 43.6 56 48 56C52.4 56 56 52.4 56 48C56 43.6 52.4 40 48 40Z"
+                  fill="white"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_135_5">
+                  <rect width="96" height="96" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </DropdownToggle>
+          <DropdownMenu {...args}>
+            <DropdownItem className={styles.dropDown} onClick={()=>console.log("hello")}>حذف</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
         <div className={styles.header}>
           <div className={styles.avatar}>{tweet.image}</div>
           <div className={styles.name}>{tweet.profile.name}</div>
@@ -29,17 +63,25 @@ function Tweet({ tweet, setOpenComment,setTweets }) {
             aria-label=""
             className={styles.buttonContainer}
           >
-            <button className={styles.button} onClick={()=>{
-              setLike(true)
-              likes(tweet.id)}}>
-              <span className={styles.icon_text}>{tweet.likes_count+like}</span>
+            <button
+              className={styles.button}
+              onClick={() => {
+                setLike(true);
+                likes(tweet.id);
+              }}
+            >
+              <span className={styles.icon_text}>
+                {tweet.likes_count + like}
+              </span>
               <i className={`tim-icons icon-heart-2 ${styles.icon}`}></i>
             </button>
             <button
               onClick={() => setOpen((x) => !x)}
               className={styles.button}
             >
-              <span className={styles.icon_text}>{tweet.replies_count - 1}</span>
+              <span className={styles.icon_text}>
+                {tweet.replies_count - 1}
+              </span>
               <i className={`far fa-comment ${styles.icon}`}></i>
             </button>
           </ButtonGroup>
@@ -50,7 +92,7 @@ function Tweet({ tweet, setOpenComment,setTweets }) {
         setOpen={() => {
           setOpen((x) => !x);
         }}
-        data = {tweet}
+        data={tweet}
         setTweets={setTweets}
       ></CommentModal>
     </>
