7134952885a0a3ee3428a0cfc938348b44b740fc
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2023-11-30 02:21:16 +0330

    feat(Admin): Protest Table Added

--- src/components/Admin/ProtestTable.jsx
+++ src/components/Admin/ProtestTable.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import Protest from './Protest'
+import * as styles from '../../assets/css/admin/Admin_Protest.module.css'
 export default function ProtestTable() {
   return (
-    <div>ProtestTable</div>
+    <>
+    {/* <div className={styles.ProtestTable_Main}> */}
+      {/* <div>ProtestTable</div> */}
+      <div className={styles.title}>گزارش ها:</div>
+      <Protest/>
+      <Protest/>
+      <Protest/>
+      {/* <Protest/> */}
+    {/* </div> */}
+    </>
   )
 }
