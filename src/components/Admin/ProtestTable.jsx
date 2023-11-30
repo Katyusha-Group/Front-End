11cb102e3a8654034b0510c3dca4104c5550585a
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2023-11-30 03:34:55 +0330

    feat(Admin): Completed Reports

--- src/components/Admin/ProtestTable.jsx
+++ src/components/Admin/ProtestTable.jsx
@@ -3,15 +3,15 @@ import Protest from './Protest'
 import * as styles from '../../assets/css/admin/Admin_Protest.module.css'
 export default function ProtestTable() {
   return (
-    <>
-    {/* <div className={styles.ProtestTable_Main}> */}
+    <div className={styles.ProtestTable_Main}>
       {/* <div>ProtestTable</div> */}
-      <div className={styles.title}>گزارش ها:</div>
-      <Protest/>
-      <Protest/>
-      <Protest/>
-      {/* <Protest/> */}
-    {/* </div> */}
-    </>
+      <div className={styles.title}>گزارش ها</div>
+      <div className={styles.protestContainer}>
+        <Protest/>
+        <Protest/>
+        <Protest/>
+        <Protest/>
+      </div>
+    </div>
   )
 }
