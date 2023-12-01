337b6a7d6b4773504522df013f6fae352558337f
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2023-12-01 15:50:02 +0330

    feat(landing) : add animations

--- src/views/NewLandingPage.jsx
+++ src/views/NewLandingPage.jsx
@@ -1,22 +1,93 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { motion, useScroll, useAnimation } from "framer-motion";
 import styles from "../assets/css/landing/newLanding.module.css";
-import { Col, Row } from "reactstrap";
-import Mainpart from "./Landing/mainpart.jsx";
-import PlanningInLanding from "./Landing/PlanningInLanding.jsx";
-import TimeLinelanding from "./Landing/TimeLinelanding.jsx";
-import ThirdLanding from "./Landing/ThirdLanding.jsx";
-import Fourthlanding from "./Landing/Fourthlanding.jsx";
-import FivethLanding from "./Landing/FivethLanding.jsx";
+import Mainpart from "./Landing/Mainpart";
+import PlanningInLanding from "./Landing/PlanningInLanding";
+import TimeLinelanding from "./Landing/TimeLinelanding";
+import ThirdLanding from "./Landing/ThirdLanding";
+import Fourthlanding from "./Landing/Fourthlanding";
+import FivethLanding from "./Landing/FivethLanding";
+
+const cardVariants  = {
+  offscreen: {
+    opacity:0,
+  },
+  onscreen: {
+    opacity:1,
+    y: 50,
+    rotate: 0,
+    transition: {
+      type: "spring",
+      bounce: 0,
+      duration: 0.8
+    }
+  }
+};
 
 const NewLandingpage = () => {
+  const { scrollY } = useScroll();
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({
+      y: scrollY,
+      transition: { duration: 0.5 },
+    });
+  }, [controls, scrollY]);
+
   return (
-    <div className={styles.landingpage}>
-      <Mainpart />
-      <PlanningInLanding />
-      <TimeLinelanding />
-      <ThirdLanding/>
-      <Fourthlanding/>
-      <FivethLanding/>
+    <div className={styles.scrollContainer}>
+      <motion.div
+        animate={controls}
+        className={styles.page}
+
+      >
+        <Mainpart />
+      </motion.div>
+
+      <motion.div
+        animate={controls}
+        className={styles.page}
+        variants={cardVariants}
+        initial="offscreen"
+        whileInView="onscreen"
+
+
+      >
+        <PlanningInLanding />
+      </motion.div>
+
+      <motion.div
+        variants={cardVariants}
+        animate={controls}
+        className={styles.page}
+        initial="offscreen"
+        whileInView="onscreen"
+
+
+      >
+        <TimeLinelanding />
+      </motion.div>
+
+      <motion.div
+        animate={controls}
+        className={styles.page}
+        variants={cardVariants}
+        initial="offscreen"
+        whileInView="onscreen"
+
+
+      >
+        <ThirdLanding />
+      </motion.div>
+
+      <motion.div animate={controls} className={styles.page}>
+        <Fourthlanding />
+      </motion.div>
+
+      <motion.div animate={controls} className={styles.page}>
+        <FivethLanding />
+      </motion.div>
     </div>
   );
 };
