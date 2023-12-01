ec467039db594752dba39dc6351762ef60020313
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2023-12-01 18:53:05 +0330

    test(landing and 404) : write some test

---
+++ src/views/Landing/mainpart.test.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  getByAltText,
+  getByRole,
+  getByTitle,
+  render,
+} from "@testing-library/react";
+import Mainpart from "./mainpart";
+import { test, it } from "vitest";
+import { expect } from "chai";
+
+test("Mainpart", () => {
+  it("renders without crashing", () => {
+    const { getByText } = render(<Mainpart />);
+    expect(getByText("کاتیوشا")).toBeInTheDocument();
+    expect(getByText("بهترین دستیار انتخاب واحد")).toBeInTheDocument();
+    expect(getByText("ثبت نام")).toBeInTheDocument();
+    expect(getByText("ورود")).toBeInTheDocument();
+    expect(
+      getByText("کاری از دانشجویان ورودی 99 دانشکده کامپیوتر")
+    ).toBeInTheDocument();
+    expect(getByText("پیشنهادات و انتقادات :")).toBeInTheDocument();
+    expect(getByText("katyushaiust@gmail.com")).toBeInTheDocument();
+  });
+});
+
+test("Mainpart", () => {
+  it("renders without crashing", () => {
+    const { getByText } = render(<Mainpart />);
+    expect(getByText("کاتیوشا")).toBeInTheDocument();
+    expect(getByText("بهترین دستیار انتخاب واحد")).toBeInTheDocument();
+    expect(getByText("ثبت نام")).toBeInTheDocument();
+  });
+});
+test("Mainpart", () => {
+  it("renders without crashing", () => {
+    const { getByText } = render(<Mainpart />);
+
+    expect(getByText("ورود")).toBeInTheDocument();
+    expect(
+      getByText("کاری از دانشجویان ورودی 99 دانشکده کامپیوتر")
+    ).toBeInTheDocument();
+    expect(getByText("پیشنهادات و انتقادات :")).toBeInTheDocument();
+    expect(getByText("katyushaiust@gmail.com")).toBeInTheDocument();
+  });
+});
+
+test("Mainpart", () => {
+  it("renders without crashing", () => {
+    const { getByText } = render(<Mainpart />);
+    expect(getByRole("button")).toBeInTheDocument();
+    expect(getByTitle("ورود")).toBeInTheDocument();
+    expect(getByAltText("ورود")).toBeInTheDocument();
+  });
+});
