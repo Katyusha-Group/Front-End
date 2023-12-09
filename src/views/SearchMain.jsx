9a95dab9bb31cbdedcf202b9d1e302c44f510154
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2023-12-09 18:54:11 +0330

    feat(searchpage) : add page and url

---
+++ src/views/SearchMain.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from '../assets/css/Search/Searchmain.module.css'
+
+const SearchMain = () => {
+  return (
+    <div className={Styles.bg}>SearchMain</div>
+  )
+}
+
+export default SearchMain
---
+++ src/views/Searchpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './Sidebar/Sidebar.jsx'
+
+const Searchpage = () => {
+  return (
+    <div>
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Searchpage
