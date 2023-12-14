35d23531d0a8bc8c120539b2297074926d140c82
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2023-12-14 18:30:45 +0330

    feat(searchpage): add search page compnents

--- src/views/Searchpage.jsx
+++ src/views/Searchpage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from './Sidebar/Sidebar.jsx'
+import SearchMain from './SearchMain.jsx'
+import styles from "../assets/css/Search/SearchPage.module.css"
 
 const Searchpage = () => {
   return (
-    <div>
-        <Sidebar/>
+    <div className={styles.main}>
+        <Sidebar/>        
+        <SearchMain/>
     </div>
   )
 }
