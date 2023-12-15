767f218446e788ed988a73173db81ee233a743bb
Yazdan Mastery Farahani <ymasteryfarahani@gmail.com>
2023-12-15 04:37:41 +0330

    Feat(Notification): Fix Bugs
    Mapping data is fixed

--- src/views/Sidebar/Notification.jsx
+++ src/views/Sidebar/Notification.jsx
@@ -3,9 +3,66 @@ import { Modal, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as styles from '../../assets/css/UsersListModal.module.css';
 import { showLoading, closeLoading } from '../../components/LoadingAlert/LoadingAlert';
-const Notification = ({ showModal, handleClose, notification }) => {
+const Notification = ({ showModal, handleClose }) => {
     const [show, setShow] = React.useState(showModal);
-
+    const notificationData = [
+        {
+            "actor": {
+                "name": "1آزمایشگاه مدارهای منطقی",
+                "username": "C_1211012",
+                "image": "http://84.32.10.112/var/www/media/images/profile_pics/course_default.png",
+                "profile_type": "C",
+                "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
+            },
+            "notification_type": "P",
+            "read": true,
+            "delta_time": "17 روز پیش",
+            "tweet_link": "http://127.0.0.1:8000/twittes/7469/",
+            "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 06 در ستون ظر فیت به مقدار 13 تغییر کرد."
+        },
+        {
+            "actor": {
+                "name": "2آزمایشگاه مدارهای منطقی",
+                "username": "C_1211012",
+                "image": "http://84.32.10.112/var/www/media/images/profile_pics/course_default.png",
+                "profile_type": "C",
+                "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
+            },
+            "notification_type": "P",
+            "read": true,
+            "delta_time": "17 روز پیش",
+            "tweet_link": "http://127.0.0.1:8000/twittes/7468/",
+            "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 01 در ستون تعداد لیست انتظار به مقدار 2 تغییر کرد."
+        },
+        {
+            "actor": {
+                "name": "3آزمایشگاه مدارهای منطقی",
+                "username": "C_1211012",
+                "image": "http://84.32.10.112/var/www/media/images/profile_pics/course_default.png",
+                "profile_type": "C",
+                "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
+            },
+            "notification_type": "P",
+            "read": true,
+            "delta_time": "17 روز پیش",
+            "tweet_link": "http://127.0.0.1:8000/twittes/7467/",
+            "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 01 در ستون تعداد ثبت نام شده به مقدار 13 تغییر کرد."
+        },
+        {
+            "actor": {
+                "name": "4آزمایشگاه مدارهای منطقی",
+                "username": "C_1211012",
+                "image": "http://84.32.10.112/var/www/media/images/profile_pics/course_default.png",
+                "profile_type": "C",
+                "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
+            },
+            "notification_type": "P",
+            "read": true,
+            "delta_time": "17 روز پیش",
+            "tweet_link": "http://127.0.0.1:8000/twittes/7466/",
+            "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 01 در ستون ظر فیت به مقدار 13 تغییر کرد."
+        }
+    ]
     // const token = JSON.parse(localStorage.getItem("authTokens")).token.access;
     React.useEffect(() => {
         showLoading();
@@ -17,7 +74,6 @@ const Notification = ({ showModal, handleClose, notification }) => {
         setShow(false);
         handleClose();
     };
-    console.log(notification)
     // console.log("Username in userlist modal is: " , username);
     return (
         <Modal show={show} onHide={handleCloseModal} className={styles.Modal}>
@@ -29,13 +85,15 @@ const Notification = ({ showModal, handleClose, notification }) => {
                     </button>
                 </Modal.Header>
                 <Modal.Body className={styles.ModalBody}>
-                    <div className={styles.eachcard}>
-                        <img className={styles.eachProfile} src={notification.actor.image} alt="" />
-                        <p className={styles.eachText}>{notification.message.split(".")[0]}. </p>
-                        <span className={styles.eachTime}>
-                            {notification.delta_time}
-                        </span>
-                    </div>
+                    {notificationData.map((notification, index) => (
+                        <div key={index} className={styles.eachcard}>
+                            <img className={styles.eachProfile} src={notification.actor.image} alt="" />
+                            <p className={styles.eachText}>{notification.message.split(".")[0]}. </p>
+                            <span className={styles.eachTime}>
+                                {notification.delta_time}
+                            </span>
+                        </div>
+                    ))}
                 </Modal.Body>
             </div>
         </Modal>
--- src/views/Sidebar/Sidebar.jsx
+++ src/views/Sidebar/Sidebar.jsx
@@ -12,65 +12,6 @@ const Sidebar = () => {
   const handleOpenModal_Notification = () => {
     setShowModal(true);
   };
-  const notificationData1 = []
-  const notificationData = [
-    {
-      "actor": {
-        "name": "1آزمایشگاه مدارهای منطقی",
-        "username": "C_1211012",
-        "image": "http://127.0.0.1:8000/var/www/media/images/profile_pics/course_default.png",
-        "profile_type": "C",
-        "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
-      },
-      "notification_type": "P",
-      "read": true,
-      "delta_time": "17 روز پیش",
-      "tweet_link": "http://127.0.0.1:8000/twittes/7469/",
-      "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 06 در ستون ظر فیت به مقدار 13 تغییر کرد."
-    },
-    {
-      "actor": {
-        "name": "2آزمایشگاه مدارهای منطقی",
-        "username": "C_1211012",
-        "image": "http://127.0.0.1:8000/var/www/media/images/profile_pics/course_default.png",
-        "profile_type": "C",
-        "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
-      },
-      "notification_type": "P",
-      "read": true,
-      "delta_time": "17 روز پیش",
-      "tweet_link": "http://127.0.0.1:8000/twittes/7468/",
-      "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 01 در ستون تعداد لیست انتظار به مقدار 2 تغییر کرد."
-    },
-    {
-      "actor": {
-        "name": "3آزمایشگاه مدارهای منطقی",
-        "username": "C_1211012",
-        "image": "http://127.0.0.1:8000/var/www/media/images/profile_pics/course_default.png",
-        "profile_type": "C",
-        "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
-      },
-      "notification_type": "P",
-      "read": true,
-      "delta_time": "17 روز پیش",
-      "tweet_link": "http://127.0.0.1:8000/twittes/7467/",
-      "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 01 در ستون تعداد ثبت نام شده به مقدار 13 تغییر کرد."
-    },
-    {
-      "actor": {
-        "name": "4آزمایشگاه مدارهای منطقی",
-        "username": "C_1211012",
-        "image": "http://127.0.0.1:8000/var/www/media/images/profile_pics/course_default.png",
-        "profile_type": "C",
-        "profile_link": "http://127.0.0.1:8000/profiles/C_1211012/"
-      },
-      "notification_type": "P",
-      "read": true,
-      "delta_time": "17 روز پیش",
-      "tweet_link": "http://127.0.0.1:8000/twittes/7466/",
-      "message": "آزمایشگاه مدارهای منطقی پست جدیدی ارسال کرد. پست او:\nگروه درسی با شماره کلاس 01 در ستون ظر فیت به مقدار 13 تغییر کرد."
-    }
-  ]
   function handleCloseModal() {
     setShowModal(false);
   };
@@ -88,27 +29,10 @@ const Sidebar = () => {
               id="toggleConfirmPassword"
             ></i>
           </p>
-          {/* {
-            notificationData && notificationData.length > 0 ?
-              notificationData.map((entry, index) => (
-                <Notification key={index} notification={entry}
-                  showModal={showModal}
-                  handleClose={handleCloseModal} />
-              )) :
-
-              <p>هیج پیامی یافت نشد</p>
-          } */}
-          {notificationData && notificationData.length > 0 &&
-            notificationData.map((entry, index) => (
-              <Notification key={index} notification={entry} showModal={showModal} handleClose={handleCloseModal} />
-            ))
-          }
-
-          {/* <Notification
-            notification={notificationData}
+          <Notification
             showModal={showModal}
             handleClose={handleCloseModal}
-          /> */}
+          />
 
         </div>
 
