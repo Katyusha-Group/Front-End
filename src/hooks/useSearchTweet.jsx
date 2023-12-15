84dd695a29579581f90026ecdfc895ca3ff7686b
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-12-15 19:53:57 +0330

    Fix(useSearchTweet): search of reports

--- src/hooks/useSearchTweet.jsx
+++ src/hooks/useSearchTweet.jsx
@@ -11,7 +11,7 @@ export const useSearchTweet = (searchQuery) => {
   const [filteredTweets, setFilteredTweets] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch(apis["reports"]["manage"]+searchQuery, {
+    fetch( apis["reports"]["manage"]+"?search="+searchQuery, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => response.json())
