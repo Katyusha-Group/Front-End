d6005f371041bcc6943fab7f07a609b1443a1c1d
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-12-29 22:06:07 +0330

    Feat(chat): add button for going

--- src/views/UserSearchResponce.jsx
+++ src/views/UserSearchResponce.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import styles from '../assets/css/UserSearchResponce.module.css'
-import {Link} from 'react-router-dom'
+import React from "react";
+import styles from "../assets/css/UserSearchResponce.module.css";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
 
 const UserSearchResponce = (res) => {
   return (
-    <Link to={"/profile/"+res.res.username} className={styles.main}>
-        <img className={styles.img} src={res.res.image} />
-        <p className={styles.text}>{res.res.name}</p>
+    <Link to={"/profile/" + res.res.username} className={styles.main}>
+      <img className={styles.img} src={res.res.image} />
+      <p className={styles.text}>{res.res.name}</p>
+      <Link to={"/chat/" + res.res.username} className={styles.chat}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+        >
+          <path
+            d="M2.5 12.8396L21.5 20.8396L17.9375 12.8396L21.5 4.8396L2.5 12.8396Z"
+            stroke="white"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M18 12.8396H2.5"
+            stroke="white"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </Link>
     </Link>
-  )
-}
+  );
+};
 
-export default UserSearchResponce
+export default UserSearchResponce;
