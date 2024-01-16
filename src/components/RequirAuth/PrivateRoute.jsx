94b42c18fbbf8a843bfb91ccc5313d493316dd4c
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2024-01-16 14:14:21 +0330

    fix(app.js) : private route add to navigate to landing

--- src/components/RequirAuth/PrivateRoute.jsx
+++ src/components/RequirAuth/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, useNavigate } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Login from '../../views/Login.jsx';
 const RequireAuth = ({ children }) => {
     const userIsLogged = !!localStorage.getItem('authTokens'); 
