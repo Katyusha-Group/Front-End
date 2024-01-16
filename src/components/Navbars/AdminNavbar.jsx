6d477556313e08cd6901face02c445a5be6bbc30
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2024-01-16 19:04:17 +0330

    style(AdminNavbar): Adminnavbar Responsive

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import routes from "../../route.jsx";
 import * as style from "../Navbars/AdminNavbar.module.css";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -11,45 +11,69 @@ import {
   Nav,
   NavItem,
   NavLink,
-  Button,
 } from 'reactstrap';
 
 function AdminNavbar() {
-  
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
     <div>
-      <Navbar>
-        <NavbarToggler type="button" onClick={toggle}  className={style.NavbarToggler}>
-        <span className="navbar-toggler-bar bar1" />
-                      <span className="navbar-toggler-bar bar2" />
-                      <span className="navbar-toggler-bar bar3" />
+      <Navbar 
+        // expand="50%"
+        className={style.AdminNavbar}
+      >
+        <NavbarToggler type="button" onClick={toggle} className={style.NavbarToggler}>
+          <span className="navbar-toggler-bar bar1" />
+          <span className="navbar-toggler-bar bar2" />
+          <span className="navbar-toggler-bar bar3" />
         </NavbarToggler>
+        <Nav className={`${style.AdminNavbar_WithoutCollapse} ml-auto`} navbar>
+          {
+            routes.map((route) =>
+              (
+                <NavItem key={route.rtlName}>
+                  <NavLink href={route.layout + route.path}>
+                    <i className={"tim-icons " + route.icon} />
+                    {" "}
+                    {route.rtlName}
+                  </NavLink>
+                </NavItem>     
+          ))}
+          <NavItem className={style.logoutLink}>
+            <NavLink href="/"
+              onClick={() => {
+              }}
+            >
+              <i className={"tim-icons icon-button-power"} />
+              {" "}
+              {"خروج"}
+            </NavLink>
+          </NavItem>
+        </Nav>
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-              {
-                routes.map((route) =>
-                  (
-                    <NavItem key={route.rtlName}>
-                      <NavLink href={route.layout + route.path}>
-                        <i className={"tim-icons " + route.icon} />
-                        {" "}
-                        {route.rtlName}
-                      </NavLink>
-                    </NavItem>     
-              ))}
-              <NavItem className={style.logoutLink}>
-                <NavLink href="/"
-                  onClick={() => {
-                  }}
-                >
-                  <i className={"tim-icons icon-button-power"} />
-                  {" "}
-                  {"خروج"}
-                </NavLink>
-              </NavItem>
-            </Nav>
+          <Nav className="ml-auto" navbar>
+            {
+              routes.map((route) =>
+                (
+                  <NavItem key={route.rtlName}>
+                    <NavLink href={route.layout + route.path}>
+                      <i className={"tim-icons " + route.icon} />
+                      {" "}
+                      {route.rtlName}
+                    </NavLink>
+                  </NavItem>     
+            ))}
+            <NavItem className={style.logoutLink}>
+              <NavLink href="/"
+                onClick={() => {
+                }}
+              >
+                <i className={"tim-icons icon-button-power"} />
+                {" "}
+                {"خروج"}
+              </NavLink>
+            </NavItem>
+          </Nav>
         </Collapse>
       </Navbar>
     </div>
