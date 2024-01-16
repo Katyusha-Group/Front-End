f58aa44e9deb8cb5b53cd344be84c3c0cde92649
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2024-01-16 12:03:42 +0330

    fix(Navbar): fixing the navabr

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -4,11 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Nav, NavLink as ReactstrapNavLink } from "reactstrap";
 import routes from "../../route.jsx";
 import * as style from "../Navbars/AdminNavbar.module.css";
-// import "../Navbars/AdminNavbar.module.css";
-import logo from "./Logo1.png";
 import { useNavigate, useLocation } from "react-router-dom";
-
-
 
 // reactstrap components
 import {
@@ -16,16 +12,18 @@ import {
   Navbar,
   NavLink,
   NavItem,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem 
 } from "reactstrap";
 
 function AdminNavbar(props) {
   const [collapseOpen, setcollapseOpen] = React.useState(false);
-  const [modalSearch, setmodalSearch] = React.useState(false);
   const [color, setcolor] = React.useState("navbar-transparent");
-  const [showLogin, setShowLogin] = useState(false);
-  const [showLoginG, setShowLoginG] = useState(false);
   const location = useLocation();
   const [selected, setSelected] = useState({});
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   useEffect(() => {
     const path = location.pathname;
@@ -60,22 +58,27 @@ function AdminNavbar(props) {
     }
     setcollapseOpen(!collapseOpen);
   };
-  // this function is to open the Search modal
-  const toggleModalSearch = () => {
-    setmodalSearch(!modalSearch);
-  };
-  const r = [1, 2, 3];
 
+  const r = [1, 2, 3];
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  // console.log(typeof(routes));
+  const First_Part = routes.slice(0, 2);
+  // console.log ("First Part is: ", First_Part);
+  const Second_Part = routes.slice(2);
+  // console.log ("Second Part is: ", Second_Part);
   return (
     <>
       <div className={style.NavbarRes}>
-        <Navbar className={classNames("navbar-absolute", color, style.rtl)} style={{direction:"rtl", position: "relative"}} expand="xs" id="navbar-absolute">
-          <Collapse navbar isOpen={collapseOpen}>
+        <Navbar className={classNames("navbar-absolute", color, style.rtl)} style={{direction:"rtl", position: "relative"}} expand="xl" id="navbar-absolute">
+          {/* <Collapse navbar isOpen={collapseOpen}> */}
             <Nav className={style.wideNavbar} navbar>
               {/* <NavLink href="/home/page" className="nav-header nav-link-icon">
                 <img src={logo} alt="" style={{ height: "34px", width: "40px" }} />
               </NavLink> */}
-              {routes.map(
+              {First_Part.map(
                 (route) => (
                   <NavItem key={route.rtlName}>
                     <NavLink
@@ -91,20 +94,74 @@ function AdminNavbar(props) {
                   </NavItem>
                 )
               )}
-              <NavItem className={style.logoutLink}>
-                <NavLink href="/"
-                  onClick={() => {
-                    localStorage.removeItem("authTokens");
-
-                  }}
-                >
-                  <i className={"tim-icons icon-button-power"} />
-                  {" "}
-                  {"خروج"}
-                </NavLink>
-              </NavItem>
+              <Collapse navbar isOpen={collapseOpen} className={style.Collapse_Main}>
+                {Second_Part.map(
+                  (route) => (
+                    <NavItem key={route.rtlName}>
+                      <NavLink
+                        href={route.layout + route.path}
+                        className={classNames({
+                          "selected": selected[route.rtlName],
+                        })}
+                      >
+                        <i className={"tim-icons " + route.icon} />
+                        {" "}
+                        {route.rtlName}
+                      </NavLink>
+                    </NavItem>
+                  )
+                )}
+                <NavItem className={style.logoutLink}>
+                  <NavLink href="/"
+                    onClick={() => {
+                    }}
+                  >
+                    <i className={"tim-icons icon-button-power"} />
+                    {" "}
+                    {"خروج"}
+                  </NavLink>
+                </NavItem>
+              </Collapse>
+              <Dropdown nav inNavbar isOpen={dropdownOpen} toggle={toggleDropdown} className={style.Dropdown}>
+                <DropdownToggle nav caret>
+                  More
+                </DropdownToggle>
+                <DropdownMenu right className={style.DropdownMenu}>
+                  {/* <DropdownItem>Item 1</DropdownItem>
+                  <DropdownItem>Item 2</DropdownItem>
+                  <DropdownItem>Item 3</DropdownItem> */}
+                  {Second_Part.map(
+                    (route) => (
+                      <DropdownItem key={route.rtlName}>
+                        <NavLink
+                          href={route.layout + route.path}
+                          // className={classNames({
+                          //   "selected": selected[route.rtlName],
+                          // })}
+                          className={style.DropdownItem}
+                        >
+                          <i className={"tim-icons " + route.icon} />
+                          {" "}
+                          {route.rtlName}
+                        </NavLink>
+                      </DropdownItem>
+                    )
+                  )}
+                <DropdownItem className={style.logoutLink}>
+                  <NavLink href="/"
+                    onClick={() => {
+                    }}
+                    className={style.DropdownItem}
+                  >
+                    <i className={"tim-icons icon-button-power"} />
+                    {" "}
+                    {"خروج"}
+                  </NavLink>
+                </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
             </Nav>
-          </Collapse>
+          {/* </Collapse> */}
         </Navbar>
       </div>
     </>
