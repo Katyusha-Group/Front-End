ec404658d2defdd91f879fc1864d0fb65a0d174e
Erfan Ebrahimi <erfan.es1381@gmail.com>
2024-01-26 14:23:05 +0330

    FIx(chat): chat with everyone now is fix

--- src/views/TimeLine/Chat.jsx
+++ src/views/TimeLine/Chat.jsx
@@ -4,7 +4,7 @@ import AdminNavbar from "../../components/Navbars/AdminNavbar.jsx";
 import styles from "../../assets/css/chat/Chat.module.css";
 import Sidebar from "../Sidebar/Sidebar.jsx";
 import Searchbar from "../Searchbar.jsx";
-import { Card, Row } from "reactstrap";
+import { Card, Row, Spinner } from "reactstrap";
 import SendMessage from "../../components/Tweet/SendMessage.jsx";
 import WebSocketInstance from "../../components/chat/ChatWebSocket.jsx";
 import { useParams } from "react-router-dom";
@@ -14,34 +14,37 @@ const ChatPage = (props) => {
   const { id } = useParams();
   const { info, changeInfo } = useInfo();
   const [chats, setChats] = React.useState([]);
+  const [chatsInfo, setChatsInfo] = React.useState(null);
   const [message, setMessage] = React.useState("");
   const [loading, setLoading] = React.useState(true);
+  let once = false;
   function waitForSocketConnection(callback) {
     const component = this;
     setTimeout(function () {
       if (WebSocketInstance.state() === 1) {
-        console.log("Connection is made");
         return;
       } else {
-        console.log("wait for connection...");
         waitForSocketConnection(callback);
       }
     }, 100);
   }
 
   useEffect(() => {
-    getChat( setChats,id,setLoading)
-    waitForSocketConnection(() => {
-      WebSocketInstance.fetchMessages(
-        // props.username,
-        // props.match.params.chatID
-        id,
-        4
-      );
-    });
-    WebSocketInstance.connect(4,setChats);
-    // WebSocketInstance.connect(props.match.params.chatID);
+    getChat(setChats, id, setLoading, setChatsInfo);
   }, []);
+
+  useEffect(() => {
+    console.log("🚀 ~ ChatPage ~ chatsInfo:", chatsInfo)
+    if(chatsInfo !== null && !once){
+      once = true
+      waitForSocketConnection(() => {
+        WebSocketInstance.fetchMessages(id, chatsInfo.id);
+      });
+      WebSocketInstance.connect(chatsInfo.id, setChats);
+    }
+  }, [chatsInfo]);
+
+  
 
   const sendMessageHandler = (e) => {
     e.preventDefault();
@@ -54,40 +57,51 @@ const ChatPage = (props) => {
     setState({ message: "" });
   };
 
-
   return (
     <Card className={styles.timeline}>
       <div className={styles.chat}>
-        {chats.map((chat, index) => (
-          <div className={` ${chat.id === info.userName? styles.chatBoxContainerRight:styles.chatBoxContainerLeft}`} key={index}>
-            <Card className={` ${styles.chatBox} ${chat.id === info.userName? styles.chatBoxRight:styles.chatBoxLeft}`}>
-              {chat.text}
-            </Card>
+        {loading ? (
+          <div>
+            <Spinner animation="border" color='red' />
           </div>
-        ))}
-        {/* <div className={`${styles.chatBoxContainerLeft}`}>
-          <Card className={` ${styles.chatBox} ${styles.chatBoxLeft}`}>
-            سلام
-          </Card>
-        </div> */}
+        ) : (
+          chats.map((chat, index) => (
+            <div
+              className={` ${
+                chat.id === info.userName
+                  ? styles.chatBoxContainerRight
+                  : styles.chatBoxContainerLeft
+              }`}
+              key={index}
+            >
+              <Card
+                className={` ${styles.chatBox} ${
+                  chat.id === info.userName
+                    ? styles.chatBoxRight
+                    : styles.chatBoxLeft
+                }`}
+              >
+                {chat.text}
+              </Card>
+            </div>
+          ))
+        )}
       </div>
       <div className={styles.sendMessage}>
         <SendMessage
-          setData={{setValue:setChats, WebSocketInstance: WebSocketInstance}}
+          setData={{ setValue: setChats, WebSocketInstance: WebSocketInstance }}
           fetchData={(myInput, setValue) => {
-            console.log("myInput", myInput)
+            console.log("myInput", myInput);
             const messageObject = {
               from: info.userName,
               content: myInput,
               chatId: 4,
             };
-            console.log("🚀 ~ file: Chat.jsx:80 ~ ChatPage ~ messageObject:", messageObject)
+            console.log(
+              "🚀 ~ file: Chat.jsx:80 ~ ChatPage ~ messageObject:",
+              messageObject
+            );
             setValue.WebSocketInstance.newChatMessage(messageObject);
-            // setValue.setValue((prev) => [{ text: myInput, id: Math.random() }, ...prev]);
-            // setValue.setValue((prev) => [
-            //   { text: myInput, id: parsedData.message.author, messageId: parsedData.message.id },
-            //   ...prev,
-            // ]);
           }}
         />
       </div>
