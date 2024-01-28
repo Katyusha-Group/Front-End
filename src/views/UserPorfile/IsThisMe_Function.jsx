ce41a305df6f9dc4ea9527d0f9945ab4c7ac0434
Erfan Ebrahimi <erfan.es1381@gmail.com>
2024-01-28 15:26:06 +0330

    Fix(MyProfile): fix bug getting profile username

--- src/views/UserPorfile/IsThisMe_Function.jsx
+++ src/views/UserPorfile/IsThisMe_Function.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { usesProfileMe } from "../../hooks/useProfileMe";
 import { GETProfileData } from "../../hooks/GETProfileData.jsx";
+import { userUserName } from "../../hooks/useUserName.jsx";
 
 export default function IsThisMe_Function (id)
 {
-    const {profile, setProfile, loading} = usesProfileMe();
+    const {profile, setProfile, loading} = userUserName();
     let IsThisMe = false;
     if (profile != null) {
         IsThisMe = (profile.username == id);
